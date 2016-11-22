e26c8024ebff62d1dbb85c2a25c73d585e4983b5
Max <446964321@qq.com>
2016-11-22 15:02:55 +0800

    update

--- setup.sh
+++ setup.sh
@@ -2,19 +2,30 @@
 
 # cleanup
 if [ -f ~/.vimrc ]; then
-  rm ~/.vimrc
+    rm ~/.vimrc
 fi
 
 if [ "$1" = "-r" ]; then
-  rm -rf ~/.vim
+    rm -rf ~/.vim
     git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+    rm -rf ~/.vim/colors
+    mkdir ~/.vim/colors
     cp ./molokai.vim ~/.vim/colors/molokai.vim
 fi
 
 cp .vimrc ~/.vimrc
 
 vim -E -s <<-EOF
-  :source ~/.vimrc
-  :qa
+:source ~/.vimrc
+:qa
 EOF
 vim +BundleInstall +qall
+
+for var in $*
+do
+    if [ "$var" = "-ycm" ]; then
+        cd ~/.vim/bundle/YouCompleteMe
+        ./install --clang-completer --system-libclang
+        cp ./.ycm_extra_conf.py ~/.ycm_extra_conf.py
+    fi
+done
