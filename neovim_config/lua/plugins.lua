2d3a3a41e0e219f8a885d1a854df8cacf0c58c27
Max <446964321@qq.com>
2021-03-24 23:25:28 +0800

    add lua support

--- neovim_config/lua/plugins.lua
+++ neovim_config/lua/plugins.lua
@@ -4,71 +4,78 @@ local cmd = vim.cmd
 -- Automatically install packer.nvim
 local install_path = fn.stdpath('data') .. '/site/pack/packer/opt/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
-    cmd('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
+        cmd('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
 end
 
 cmd [[packadd packer.nvim]]
 require('packer').startup(function(use)
-    -- Packer itself
-    use {'wbthomason/packer.nvim', opt = true}
+        -- Packer itself
+        use {'wbthomason/packer.nvim', opt = true}
 
-    use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
-    use {'windwp/nvim-ts-autotag'}
-    use {'JoosepAlviste/nvim-ts-context-commentstring'}
+        use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
+        use {'windwp/nvim-ts-autotag'}
+        use {'JoosepAlviste/nvim-ts-context-commentstring'}
 
-    use {'jiangmiao/auto-pairs'}
+        use {'jiangmiao/auto-pairs'}
 
-    use {'PangPangPangPangPang/visual-selection-vim'}
-    use {'mhinz/vim-grepper', cmd = {'GrepperRg', 'Grepper'}}
+        -- Indentation tracking
+        use {
+                'lukas-reineke/indent-blankline.nvim',
+                branch = 'lua',
+                setup = [[require('config.indentline')]]
+        }
 
-    use {'neoclide/coc.nvim', branch = 'release'}
+        use {'PangPangPangPangPang/visual-selection-vim'}
+        use {'mhinz/vim-grepper', cmd = {'GrepperRg', 'Grepper'}}
 
-
-    use {'ryanoasis/vim-devicons'}
-    vim.g.devicons_install = 1
-    use {'tomasiser/vim-code-dark'}
-
-    use {'tpope/vim-rsi'}
-
-    use {'romainl/vim-cool'}
-
-    use {'machakann/vim-highlightedyank'}
-
-    use {'liuchengxu/vista.vim', cmd = {'Vista'}}
-
-    use {'Shougo/defx.nvim', requires = {'kristijanhusak/defx-icons', 'kristijanhusak/defx-git'}}
-
-    use {'tweekmonster/startuptime.vim', cmd = 'StartupTime'}
+        use {'neoclide/coc.nvim', branch = 'release'}
 
 
-    use {'kyazdani42/nvim-web-devicons'}
-    use {'glepnir/galaxyline.nvim'}
+        use {'ryanoasis/vim-devicons'}
+        vim.g.devicons_install = 1
+        use {'tomasiser/vim-code-dark'}
 
-    use {'nvim-lua/popup.nvim'}
-    use {'nvim-lua/plenary.nvim'}
-    use {'nvim-telescope/telescope.nvim'}
+        use {'tpope/vim-rsi'}
 
-    use {'iamcco/markdown-preview.nvim', run = 'cd app && yarn install', cmd = {'MarkdownPreview'}}
+        use {'romainl/vim-cool'}
 
-    use {'szw/vim-maximizer', cmd = {'MaximizerToggle'}}
+        use {'machakann/vim-highlightedyank'}
 
-    use {'airblade/vim-gitgutter'}
-    use {'tpope/vim-fugitive'}
-    use {'junegunn/gv.vim'}
-    use {'rhysd/git-messenger.vim'}
-    use {'honza/vim-snippets'}
-    use {'rhysd/clever-f.vim'}
-    use {'kshenoy/vim-signature'}
-    use {'simnalamburt/vim-mundo'}
-    use {'skywind3000/vim-terminal-help'}
-    use {'rrethy/vim-hexokinase', run = 'make hexokinase'}
-    use {'RRethy/vim-illuminate'}
-    use {'tpope/vim-dadbod', cmd = {'DB'}}
-    use {'skywind3000/vim-quickui'}
-    use {'kana/vim-textobj-user'}
-    use {'kana/vim-textobj-function'}
-    use {'kana/vim-textobj-indent'}
-    use {'kana/vim-textobj-line'}
+        use {'liuchengxu/vista.vim', cmd = {'Vista'}}
+
+        use {'Shougo/defx.nvim', requires = {'kristijanhusak/defx-icons', 'kristijanhusak/defx-git'}}
+
+        use {'tweekmonster/startuptime.vim', cmd = 'StartupTime'}
+
+
+        use {'kyazdani42/nvim-web-devicons'}
+        use {'glepnir/galaxyline.nvim'}
+
+        use {'nvim-lua/popup.nvim'}
+        use {'nvim-lua/plenary.nvim'}
+        use {'nvim-telescope/telescope.nvim'}
+
+        use {'iamcco/markdown-preview.nvim', run = 'cd app && yarn install', cmd = {'MarkdownPreview'}}
+
+        use {'szw/vim-maximizer', cmd = {'MaximizerToggle'}}
+
+        use {'airblade/vim-gitgutter'}
+        use {'tpope/vim-fugitive'}
+        use {'junegunn/gv.vim'}
+        use {'rhysd/git-messenger.vim'}
+        use {'honza/vim-snippets'}
+        use {'rhysd/clever-f.vim'}
+        use {'kshenoy/vim-signature'}
+        use {'simnalamburt/vim-mundo'}
+        use {'skywind3000/vim-terminal-help'}
+        use {'rrethy/vim-hexokinase', run = 'make hexokinase'}
+        use {'RRethy/vim-illuminate'}
+        use {'tpope/vim-dadbod', cmd = {'DB'}}
+        use {'skywind3000/vim-quickui'}
+        use {'kana/vim-textobj-user'}
+        use {'kana/vim-textobj-function'}
+        use {'kana/vim-textobj-indent'}
+        use {'kana/vim-textobj-line'}
 end)
 
 vim.cmd [[colorscheme codedark]]
