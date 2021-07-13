d312301ec2871b0dd79c4895bbe1dd41706b5a3e
PangPangPangPangPang <446964321@qq.com>
2021-07-13 17:50:11 +0800

    feat: open mfsu for debug

--- neovim_config/lua/lsp_config.lua
+++ neovim_config/lua/lsp_config.lua
@@ -1,95 +1,99 @@
--- require'snippets'.use_suggested_mappings()
-local nvim_lsp = require('lspconfig')
+local M = {}
+M.config = function()
+    -- require'snippets'.use_suggested_mappings()
+    local nvim_lsp = require('lspconfig')
 
-local on_attach = function(client, bufnr)
-    local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-    local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
-    buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
-    -- Mappings.
-    -- lsp
-    local opts = { noremap=true, silent=true }
-    buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-    buf_set_keymap('n', '<c-]>', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-    -- buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-    buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-    buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-    buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-    buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-    buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-    buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-    buf_set_keymap('n', '<space>cn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-    buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-    buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-    buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
-    buf_set_keymap('n', '<space>cp', ':Prettier<CR>', opts)
-    -- buf_set_keymap('n', '<space>cd', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-    -- buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-    -- buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+    local on_attach = function(client, bufnr)
+        local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+        local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+        buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+        -- Mappings.
+        -- lsp
+        local opts = { noremap=true, silent=true }
+        buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+        buf_set_keymap('n', '<c-]>', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+        -- buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+        buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+        buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+        buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+        buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+        buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+        buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+        buf_set_keymap('n', '<space>cn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+        buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+        buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+        buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+        buf_set_keymap('n', '<space>cp', ':Prettier<CR>', opts)
+        -- buf_set_keymap('n', '<space>cd', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+        -- buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+        -- buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
 
-    -- Set some keybinds conditional on server capabilities
-    -- if client.resolved_capabilities.document_formatting then
-    --   buf_set_keymap("n", "<space>cp", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
-    -- end
-    -- if client.resolved_capabilities.document_range_formatting then
-    --   buf_set_keymap("v", "<space>cp", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
-    -- end
+        -- Set some keybinds conditional on server capabilities
+        -- if client.resolved_capabilities.document_formatting then
+        --   buf_set_keymap("n", "<space>cp", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+        -- end
+        -- if client.resolved_capabilities.document_range_formatting then
+        --   buf_set_keymap("v", "<space>cp", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
+        -- end
 
-    -- lspsaga
-    -- buf_set_keymap('n', 'K', "<cmd>lua require('lspsaga.hover').render_hover_doc()<CR>", opts)
-    buf_set_keymap('n', '<space>cn', "<cmd>lua require('lspsaga.rename').rename()<CR>", opts)
-    buf_set_keymap('n', 'K', "<cmd>lua require('lspsaga.hover').render_hover_doc()<CR>", opts)
-    buf_set_keymap('n', '<space>cd', "<cmd>lua require'lspsaga.diagnostic'.show_line_diagnostics()<CR>", opts)
-    buf_set_keymap('n', '<space>c]', "<cmd>lua require'lspsaga.diagnostic'.lsp_jump_diagnostic_prev()<CR>", opts)
-    buf_set_keymap('n', '<space>c[', "<cmd>lua require'lspsaga.diagnostic'.lsp_jump_diagnostic_next()<CR>", opts)
+        -- lspsaga
+        -- buf_set_keymap('n', 'K', "<cmd>lua require('lspsaga.hover').render_hover_doc()<CR>", opts)
+        buf_set_keymap('n', '<space>cn', "<cmd>lua require('lspsaga.rename').rename()<CR>", opts)
+        buf_set_keymap('n', 'K', "<cmd>lua require('lspsaga.hover').render_hover_doc()<CR>", opts)
+        buf_set_keymap('n', '<space>cd', "<cmd>lua require'lspsaga.diagnostic'.show_line_diagnostics()<CR>", opts)
+        buf_set_keymap('n', '<space>c]', "<cmd>lua require'lspsaga.diagnostic'.lsp_jump_diagnostic_prev()<CR>", opts)
+        buf_set_keymap('n', '<space>c[', "<cmd>lua require'lspsaga.diagnostic'.lsp_jump_diagnostic_next()<CR>", opts)
 
-    -- Set autocommands conditional on server_capabilities
-    if client.resolved_capabilities.document_highlight then
-        vim.api.nvim_exec([[
-        augroup lsp_document_highlight
-        autocmd! * <buffer>
-        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+        -- Set autocommands conditional on server_capabilities
+        if client.resolved_capabilities.document_highlight then
+            vim.api.nvim_exec([[
+            augroup lsp_document_highlight
+            autocmd! * <buffer>
+            autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+            autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
 
-        autocmd CursorHold <buffer> lua require'lspsaga.diagnostic'.show_cursor_diagnostics()
-        augroup END
-        highlight LspReference guifg=NONE guibg=#444444 guisp=NONE gui=NONE cterm=NONE ctermfg=NONE ctermbg=59
-        highlight! link LspReferenceText LspReference
-        highlight! link LspReferenceRead LspReference
-        highlight! link LspReferenceWrite LspReference
+            autocmd CursorHold <buffer> lua require'lspsaga.diagnostic'.show_cursor_diagnostics()
+            augroup END
+            highlight LspReference guifg=NONE guibg=#444444 guisp=NONE gui=NONE cterm=NONE ctermfg=NONE ctermbg=59
+            highlight! link LspReferenceText LspReference
+            highlight! link LspReferenceRead LspReference
+            highlight! link LspReferenceWrite LspReference
 
-        ]], false)
+            ]], false)
+        end
+        vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+        vim.lsp.diagnostic.on_publish_diagnostics, {
+            -- Enable underline, use default values
+            underline = true,
+            -- Enable virtual text, override spacing to 4
+            virtual_text = {
+                spacing = 4,
+            },
+            -- Disable a feature
+            update_in_insert = false,
+        }
+        )
     end
-    vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-    vim.lsp.diagnostic.on_publish_diagnostics, {
-        -- Enable underline, use default values
-        underline = true,
-        -- Enable virtual text, override spacing to 4
-        virtual_text = {
-            spacing = 4,
-        },
-        -- Disable a feature
-        update_in_insert = false,
+
+    -- Use a loop to conveniently both setup defined servers 
+    -- and map buffer local keybindings when the language server attaches
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    capabilities.textDocument.completion.completionItem.snippetSupport = true
+    capabilities.textDocument.completion.completionItem.resolveSupport = {
+        properties = {
+            'documentation',
+            'detail',
+            'additionalTextEdits',
+        }
     }
-    )
+
+    local servers = { "pyright", "rust_analyzer", "tsserver", "cssls", "stylelint_lsp", "html" }
+    for _, lsp in ipairs(servers) do
+        nvim_lsp[lsp].setup { 
+            on_attach = on_attach,
+            capabilities = capabilities,
+        }
+    end
+
 end
-
--- Use a loop to conveniently both setup defined servers 
--- and map buffer local keybindings when the language server attaches
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.completion.completionItem.snippetSupport = true
-capabilities.textDocument.completion.completionItem.resolveSupport = {
-    properties = {
-        'documentation',
-        'detail',
-        'additionalTextEdits',
-    }
-}
-
-local servers = { "pyright", "rust_analyzer", "tsserver", "cssls", "stylelint_lsp", "html" }
-for _, lsp in ipairs(servers) do
-    nvim_lsp[lsp].setup { 
-        on_attach = on_attach,
-        capabilities = capabilities,
-    }
-end
-
+return M
--- neovim_config/lua/nvim_compe.lua
+++ neovim_config/lua/nvim_compe.lua
@@ -3,28 +3,68 @@
 buf_set_keymap('i', '<c-e>', "<cmd>lua compe#confirm('<c-e>')", opts)
 buf_set_keymap('i', '<c-n>', "<cmd>lua compe#scroll({ 'delta': +1 })", opts)
 buf_set_keymap('i', '<c-p>', "<cmd>lua compe#scroll({ 'delta': -1 })", opts) ]]
-require'compe'.setup {
-    enabled = true;
-    autocomplete = true;
-    debug = false;
-    min_length = 1;
-    -- preselect = 'enable';
-    preselect = 'always';
-    throttle_time = 80;
-    source_timeout = 200;
-    incomplete_delay = 400;
-    max_abbr_width = 100;
-    max_kind_width = 100;
-    max_menu_width = 100;
-    documentation = true;
+-- require'compe'.setup {
+--     enabled = true;
+--     autocomplete = true;
+--     debug = false;
+--     min_length = 1;
+--     -- preselect = 'enable';
+--     preselect = 'always';
+--     throttle_time = 80;
+--     source_timeout = 200;
+--     incomplete_delay = 400;
+--     max_abbr_width = 100;
+--     max_kind_width = 100;
+--     max_menu_width = 100;
+--     documentation = true;
 
-    source = {
-        vsnip = true;
-        ultisnips = true;
-        nvim_lsp = true;
-        nvim_lua = true;
-        path = true;
-        buffer = true;
-        calc = true;
-    };
-}
+--     source = {
+--         vsnip = true;
+--         ultisnips = true;
+--         nvim_lsp = true;
+--         nvim_lua = true;
+--         path = true;
+--         buffer = true;
+--         calc = true;
+--     };
+-- }
+local M = {}
+
+M.config = function()
+    require "compe".setup {
+        enabled = true,
+        autocomplete = true,
+        debug = false,
+        min_length = 1,
+        preselect = "always",
+        throttle_time = 80,
+        source_timeout = 200,
+        incomplete_delay = 400,
+        max_abbr_width = 100,
+        max_kind_width = 100,
+        max_menu_width = 100,
+        documentation = true,
+        source = {
+            luasnip = {kind = "﬌", true},
+            nvim_lsp = true,
+            nvim_lua = true,
+            path = true;
+            buffer = {kind = "﬘", true},
+            calc = true;
+        }
+    }
+end
+
+M.snippets = function()
+    local ls = require("luasnip")
+
+    ls.config.set_config(
+        {
+            history = true,
+            updateevents = "TextChanged,TextChangedI"
+        }
+    )
+    require("luasnip/loaders/from_vscode").load()
+end
+
+return M
