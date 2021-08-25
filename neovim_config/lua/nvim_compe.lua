762c88486ad2ca43f3f1e5e971d4cd04e6c4fd63
PangPangPangPangPang <446964321@qq.com>
2021-08-25 15:20:17 +0800

    update

--- neovim_config/lua/nvim_compe.lua
+++ neovim_config/lua/nvim_compe.lua
@@ -10,18 +10,26 @@ M.config = function()
     -- map('i', '<C-f>', "compe#scroll({ 'delta': +4 })", opts)
     -- map('i', '<C-d>', "compe#scroll({ 'delta': -4 })", opts)
     require"compe".setup {
-        enabled = true,
-        autocomplete = true,
-        debug = false,
-        min_length = 1,
-        preselect = "always",
-        throttle_time = 80,
-        source_timeout = 200,
-        incomplete_delay = 400,
-        max_abbr_width = 100,
-        max_kind_width = 100,
-        max_menu_width = 100,
-        documentation = true,
+        enabled = true;
+        autocomplete = true;
+        debug = false;
+        min_length = 1;
+        preselect = 'enable';
+        throttle_time = 80;
+        source_timeout = 200;
+        resolve_timeout = 800;
+        incomplete_delay = 400;
+        max_abbr_width = 100;
+        max_kind_width = 100;
+        max_menu_width = 100;
+        documentation = {
+            border = { '', '' ,'', ' ', '', '', '', ' ' }, -- the border option is the same as `|help nvim_open_win|`
+            winhighlight = "NormalFloat:CompeDocumentation,FloatBorder:CompeDocumentationBorder",
+            max_width = 120,
+            min_width = 60,
+            max_height = math.floor(vim.o.lines * 0.3),
+            min_height = 1,
+        };
         source = {
             luasnip = {kind = "﬌", true},
             nvim_lsp = true,
