f3817976e35aba2c7d2b3756cda12bcc411b336c
PangPangPangPangPang <446964321@qq.com>
2021-08-27 22:48:31 +0800

    update

--- neovim_config/lua/_zfvimim.lua
+++ neovim_config/lua/_zfvimim.lua
@@ -4,20 +4,12 @@ M.setup = function ()
     local map = require('utils').map
     local opts = {noremap = true, silent = true, expr = true}
     map('i', '<c-i>', 'ZFVimIME_keymap_toggle_i()', opts)
-    -- vim.api.nvim_exec([[
-    -- function! s:myLocalDb()
-    -- let db = ZFVimIM_dbInit({
-    --     \   'name' : 'YourDb',
-    --     \ })
-    --     call ZFVimIM_cloudRegister({
-    --         \   'mode' : 'local',
-    --         \   'dbId' : db['dbId'],
-    --         \   'repoPath' : '~/.config/nvim', 
-    --         \   'dbFile' : '/pinyin.txt', 
-    --         \ })
-    --         endfunction
-    --         " autocmd User ZFVimIM_event_OnDbInit call s:myLocalDb()
+    M.setLocalDB();
+end
 
-    --         ]], false)
+M.setLocalDB = function ()
+    -- local db = vim.api.nvim_call_function('ZFVimIM_dbInit', {name = 'MaxDB'})
+    local db = vim.fn.ZFVimIM_dbInit({name = 'MaxDB'})
+    vim.fn.ZFVimIM_cloudRegister({mode = 'local', dbId = db['dbId'], repoPath = '~/.config/nvim', dbFile = '/pinyin.txt'})
 end
 return M
