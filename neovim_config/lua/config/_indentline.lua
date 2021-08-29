e78b06b0982cca2b141343f4d530faea9b20e847
PangPangPangPangPang <446964321@qq.com>
2021-08-29 16:42:41 +0800

    update

--- neovim_config/lua/config/_indentline.lua
+++ neovim_config/lua/config/_indentline.lua
@@ -11,7 +11,7 @@ M.setup = function ()
         buftype_exclude = {'tex', 'markdown', 'txt', 'startify', 'packer'},
         show_end_of_line = true,
         space_char_blankline = " ",
-        -- show_current_context = true,
+        show_current_context = true,
     }
 end
 return M
