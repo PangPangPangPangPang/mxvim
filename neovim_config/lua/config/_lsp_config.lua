b1c0bd68e6bde549e2cf925b810cc21a0ceb8130
PangPangPangPangPang <446964321@qq.com>
2021-09-03 11:04:46 +0800

    update

--- neovim_config/lua/config/_lsp_config.lua
+++ neovim_config/lua/config/_lsp_config.lua
@@ -172,20 +172,23 @@ M.signature = function()
             bind = true,
             doc_lines = 2,
             floating_window = true,
-            fix_pos = true,
+            fix_pos = false,
             hint_enable = true,
-            hint_prefix = " ",
+            hint_prefix = "🐼 ",  -- Panda for parameter
             hint_scheme = "String",
-            transpancy = 20,
+            transpancy = 50,
             use_lspsaga = false,
             hi_parameter = "Search",
             max_height = 22,
             max_width = 120, -- max_width of signature floating_window, line will be wrapped if exceed max_width
             handler_opts = {
-                border = "single" -- double, single, shadow, none
+                border = "shadow" -- double, single, shadow, none
             },
             zindex = 200, -- by default it will be on top of all floating windows, set to 50 send it to bottom
-            padding = "" -- character to pad on left and right of signature can be ' ', or '|'  etc
+            padding = "", -- character to pad on left and right of signature can be ' ', or '|'  etc
+            shadow_blend = 36, -- if you using shadow as border use this set the opacity
+            shadow_guibg = 'Black', -- if you using shadow as border use this set the color e.g. 'Green' or '#121315'
+
         }
     end
 end
