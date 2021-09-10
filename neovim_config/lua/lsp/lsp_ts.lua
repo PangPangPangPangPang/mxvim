3d4cbbe8a2789b0e946d222fc584d5854019ba78
PangPangPangPangPang <446964321@qq.com>
2021-09-10 18:00:35 +0800

    update

--- neovim_config/lua/lsp/lsp_ts.lua
+++ neovim_config/lua/lsp/lsp_ts.lua
@@ -3,7 +3,7 @@ local config = require('config._lsp_config')
 M.on_attach = function(client, bufnr)
     config.set_keymap(client, bufnr)
     config.set_signature(bufnr)
-    client.resolved_capabilities.document_formatting = false
-    client.resolved_capabilities.document_range_formatting = false
+    -- client.resolved_capabilities.document_formatting = false
+    -- client.resolved_capabilities.document_range_formatting = false
 end
 return M
