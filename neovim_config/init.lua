a25ec09e5db3d064c12cb9ff6fab6b8ffd0b68e2
PangPangPangPangPang <446964321@qq.com>
2021-09-16 11:56:18 +0800

    update

--- neovim_config/init.lua
+++ neovim_config/init.lua
@@ -1,4 +1,5 @@
 require('basic')
+require('plugins')
 
 local fn = vim.fn
 local cmd = vim.cmd
@@ -6,15 +7,14 @@ local uv = vim.loop
 local conf_dir = fn.stdpath('config')
 if uv.fs_stat(conf_dir .. '/plugin/packer_compiled.lua') then
     cmd([[
-    com! PackerInstall lua require('plugs.packer').install()
-    com! PackerUpdate lua require('plugs.packer').update()
-    com! PackerSync lua require('plugs.packer').sync()
-    com! PackerClean lua require('plugs.packer').clean()
-    com! PackerStatus lua require('plugs.packer').status()
-    com! -nargs=? PackerCompile lua require('plugs.packer').compile(<q-args>)
-    com! -nargs=+ PackerLoad lua require('plugs.packer').loader(<q-args>)
+    com! PackerInstall lua require('packer').install()
+    com! PackerUpdate lua require('packer').update()
+    com! PackerSync lua require('packer').sync()
+    com! PackerClean lua require('packer').clean()
+    com! PackerStatus lua require('packer').status()
+    com! -nargs=? PackerCompile lua require('packer').compile(<q-args>)
+    com! -nargs=+ PackerLoad lua require('packer').loader(<q-args>)
     ]])
 else
     require('packer').compile()
 end
-require('plugins')
