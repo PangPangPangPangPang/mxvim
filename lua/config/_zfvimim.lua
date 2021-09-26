701cfb438639d5003b29fc990c45a15d12945b32
PangPangPangPangPang <446964321@qq.com>
2021-09-26 23:22:57 +0800

    update

--- lua/config/_zfvimim.lua
+++ lua/config/_zfvimim.lua
@@ -5,13 +5,27 @@ M.setup = function ()
     vim.cmd([[
         let g:ZFVimIM_symbolMap = {'`' : ['·'],'!' : ['！'],'$' : ['￥'],'^' : ['……'],'-' : [''],'_' : ['——'],'(' : ['（'],')' : ['）'],'[' : ['【'],']' : ['】'],'<' : ['《'],'>' : ['》'],'\' : ['、'],'/' : ['、'],';' : ['；'],':' : ['：'],',' : ['，'],'.' : ['。'],'?' : ['？'],"'" : ['‘', '’'],'"' : ['“', '”'],}
     ]])
-    if vim.o.filetype == 'markdown' then
-        vim.cmd([[ PackerLoad ZFVimIM ]])
-    end
+
+    vim.schedule(function ()
+        if vim.o.filetype == 'markdown' then
+            vim.cmd([[ PackerLoad ZFVimIM ]])
+        end
+    end)
     local map = require('utils').map
     local opts = {noremap = true, silent = true}
     map('i', ';;', "<C-o>:lua toggle_zfvimim()<cr>", opts)
+    vim.cmd([[
+        autocmd User TelescopeFindPre lua stop_zfvimim()
+    ]])
 end
+function stop_zfvimim()
+    if packer_plugins["ZFVimIM"] and packer_plugins["ZFVimIM"].loaded then
+        if vim.fn.ZFVimIME_started() == 1 then
+            vim.fn.ZFVimIME_stop()
+        end
+    end
+end
+
 function toggle_zfvimim()
     if packer_plugins["ZFVimIM"] and packer_plugins["ZFVimIM"].loaded then
     else
@@ -25,6 +39,7 @@ end
 
 
 M.setLocalDB = function ()
+
     local fn = vim.fn
     if require('utils').is_module_available('private') == true then
         local db = fn.ZFVimIM_dbInit({name = 'MaxDB'})
