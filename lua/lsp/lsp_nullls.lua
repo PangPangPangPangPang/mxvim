8d6d07e4bff492791d1eb17a60f5a4f447f1c311
PangPangPangPangPang <446964321@qq.com>
2021-09-30 11:18:40 +0800

    update

--- lua/lsp/lsp_nullls.lua
+++ lua/lsp/lsp_nullls.lua
@@ -1,28 +1,28 @@
 local M = {}
 M.setup = function()
-    vim.defer_fn(function ()
-        vim.cmd([[ PackerLoad null-ls.nvim]])
-    end, 1000)
+	vim.defer_fn(function()
+		vim.cmd([[ PackerLoad null-ls.nvim]])
+	end, 1000)
 end
-M.config = function ()
-    local lspconfig = require('lspconfig')
-    local config = require('config._lsp_config').make_config()
-    local null_ls = require("null-ls")
-    -- pip3 install codespell
-    -- yarn add global eslint_d
-    -- luarocks install luacheck
-    local prettier = null_ls.builtins.formatting.prettier
-    prettier.filetypes = { "css", "scss", "html", "json", "yaml", "markdown" }
+M.config = function()
+	local lspconfig = require("lspconfig")
+	local config = require("config._lsp_config").make_config()
+	local null_ls = require("null-ls")
+	-- pip3 install codespell
+	-- yarn add global eslint_d
+	-- luarocks install luacheck
+	local prettier = null_ls.builtins.formatting.prettier
+	prettier.filetypes = { "css", "scss", "html", "json", "yaml", "markdown" }
 
-    local sources = {
-        null_ls.builtins.diagnostics.eslint_d,
-        null_ls.builtins.diagnostics.codespell,
-        -- null_ls.builtins.diagnostics.luacheck,
-        null_ls.builtins.formatting.eslint_d,
-        null_ls.builtins.formatting.stylua,
-        prettier,
-    }
-    null_ls.config({sources = sources, debug = false})
-    lspconfig["null-ls"].setup(config)
+	local sources = {
+		null_ls.builtins.diagnostics.eslint_d,
+		null_ls.builtins.diagnostics.codespell,
+		null_ls.builtins.diagnostics.luacheck,
+		null_ls.builtins.formatting.eslint_d,
+		null_ls.builtins.formatting.stylua,
+		prettier,
+	}
+	null_ls.config({ sources = sources, debug = false })
+	lspconfig["null-ls"].setup(config)
 end
-return M;
+return M
