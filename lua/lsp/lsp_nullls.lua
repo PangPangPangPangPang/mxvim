cf4bbd60f5b97eab5525690154e3029537026525
PangPangPangPangPang <446964321@qq.com>
2021-10-08 18:26:10 +0800

    update

--- lua/lsp/lsp_nullls.lua
+++ lua/lsp/lsp_nullls.lua
@@ -30,6 +30,7 @@ M.config = function()
 	}
 
 	local sources = {
+        null_ls.builtins.code_actions.gitsigns,
 		null_ls.builtins.diagnostics.eslint_d,
 		null_ls.builtins.diagnostics.codespell,
 		luacheck,
