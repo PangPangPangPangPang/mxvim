df2a3cd7b8ec3ea56ccb7d80e5295f76e6f48c85
PangPangPangPangPang <446964321@qq.com>
2021-10-08 18:42:29 +0800

    update

--- lua/config/_autopairs.lua
+++ lua/config/_autopairs.lua
@@ -1,17 +1,53 @@
 local M = {}
-M.setup = function ()
-    vim.g.AutoPairsFlyMode = 0
-    vim.g.AutoPairsMultilineClose = 0
-    require('nvim-autopairs').setup{}
-    require("nvim-autopairs.completion.cmp").setup({
-        map_cr = true, --  map <CR> on insert mode
-        map_complete = true, -- it will auto insert `(` (map_char) after select function or method item
-        auto_select = true, -- automatically select the first item
-        insert = false, -- use insert confirm behavior instead of replace
-        map_char = { -- modifies the function or method delimiter by filetypes
-        all = '(',
-        tex = '{'
-    }
-})
+M.setup = function()
+	vim.g.AutoPairsFlyMode = 0
+	vim.g.AutoPairsMultilineClose = 0
+	require("nvim-autopairs").setup({
+		disable_filetype = { "TelescopePrompt", "vim" },
+	})
+	local npairs = require("nvim-autopairs")
+	local Rule = require("nvim-autopairs.rule")
+
+	npairs.add_rules({
+		Rule(" ", " "):with_pair(function(opts)
+			local pair = opts.line:sub(opts.col - 1, opts.col)
+			return vim.tbl_contains({ "()", "[]", "{}" }, pair)
+		end),
+		Rule("( ", " )")
+			:with_pair(function()
+				return false
+			end)
+			:with_move(function(opts)
+				return opts.prev_char:match(".%)") ~= nil
+			end)
+			:use_key(")"),
+		Rule("{ ", " }")
+			:with_pair(function()
+				return false
+			end)
+			:with_move(function(opts)
+				return opts.prev_char:match(".%}") ~= nil
+			end)
+			:use_key("}"),
+		Rule("[ ", " ]")
+			:with_pair(function()
+				return false
+			end)
+			:with_move(function(opts)
+				return opts.prev_char:match(".%]") ~= nil
+			end)
+			:use_key("]"),
+	})
+
+	require("nvim-autopairs.completion.cmp").setup({
+		map_cr = true, --  map <CR> on insert mode
+		map_complete = true, -- it will auto insert `(` (map_char) after select function or method item
+		auto_select = true, -- automatically select the first item
+		insert = false, -- use insert confirm behavior instead of replace
+		map_char = { -- modifies the function or method delimiter by filetypes
+			all = "(",
+			tex = "{",
+		},
+	})
 end
 return M
