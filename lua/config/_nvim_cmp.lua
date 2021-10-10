b561ebf5478034a8f45489b75ad0e4b6f029f0b5
PangPangPangPangPang <446964321@qq.com>
2021-10-10 15:31:53 +0800

    update

--- lua/config/_nvim_cmp.lua
+++ lua/config/_nvim_cmp.lua
@@ -1,62 +1,68 @@
 local M = {}
 
 M.setup = function()
-	vim.defer_fn(function()
-		vim.cmd([[
-            PackerLoad lspkind-nvim
-            PackerLoad nvim-cmp
-            " Jump forward or backward
-            imap <expr> <Tab>   vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<Tab>'
-            smap <expr> <Tab>   vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<Tab>'
-            imap <expr> <S-Tab> vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'
-            smap <expr> <S-Tab> vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'
+    vim.defer_fn(function()
+        vim.cmd([[
+        PackerLoad lspkind-nvim
+        PackerLoad nvim-cmp
+        " Jump forward or backward
+        imap <expr> <Tab>   vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<Tab>'
+        smap <expr> <Tab>   vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<Tab>'
+        imap <expr> <S-Tab> vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'
+        smap <expr> <S-Tab> vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'
         ]])
-	end, 400)
+    end, 400)
 end
 
 M.config = function()
-	local cmp = require("cmp")
-	local lspkind = require("lspkind")
-	cmp.setup({
-		snippet = {
-			expand = function(args)
-				vim.fn["vsnip#anonymous"](args.body)
-			end,
-		},
+    local cmp = require("cmp")
+    local lspkind = require("lspkind")
+    cmp.setup({
+        snippet = {
+            expand = function(args)
+                vim.fn["vsnip#anonymous"](args.body)
+            end,
+        },
         experimental = {
             native_menu = false,
             ghost_text = { hl_group = 'Comment' }
         },
         -- preselect = cmp.PreselectMode.None,
-		-- You can set mapping if you want.
-		mapping = {
+        -- You can set mapping if you want.
+        mapping = {
             ['<C-n>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
             ['<C-p>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
-			['<C-d>'] = cmp.mapping.scroll_docs(-4),
-			['<C-f>'] = cmp.mapping.scroll_docs(4),
-			["<C-Space>"] = cmp.mapping.complete(),
-			["<C-e>"] = cmp.mapping.close(),
-			['<CR>'] = cmp.mapping.confirm({
-			    behavior = cmp.ConfirmBehavior.Replace,
-			    select = true,
-			})
-		},
-		-- You should specify your *installed* sources.
-		sources = {
-			{ name = "vsnip" },
-			{ name = "nvim_lsp" },
-			-- { name = "tags" },
-			{ name = "nvim_lua" },
-			{ name = "buffer" },
-			{ name = "path" },
-		},
-		completion = {
-			completeopt = "menu,menuone,noinsert",
-			keyword_length = 1,
-		},
-        formatting = {
-            format = lspkind.cmp_format({with_text = true, maxwidth = 50})
+            ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+            ['<C-f>'] = cmp.mapping.scroll_docs(4),
+            ["<C-Space>"] = cmp.mapping.complete(),
+            ["<C-e>"] = cmp.mapping.close(),
+            ['<CR>'] = cmp.mapping.confirm({
+                behavior = cmp.ConfirmBehavior.Replace,
+                select = true,
+            })
         },
-	})
+        -- You should specify your *installed* sources.
+        sources = {
+            { name = "vsnip" },
+            { name = "nvim_lsp" },
+            -- { name = "tags" },
+            { name = "nvim_lua" },
+            { name = "buffer" },
+            { name = "path" },
+        },
+        completion = {
+            completeopt = "menu,menuone,noinsert",
+            keyword_length = 1,
+        },
+        formatting = {
+            format = require("lspkind").cmp_format({with_text = true, menu = ({
+                vsnip = "[Snip]",
+                nvim_lsp = "[LSP]",
+                nvim_lua = "[Lua]",
+                buffer = "[Buffer]",
+                path = "[Path]",
+            })}),
+        },
+    })
 end
 return M
