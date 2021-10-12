17437a080faf35af361f89dc08d2cc0d743fd954
PangPangPangPangPang <446964321@qq.com>
2021-10-12 23:47:14 +0800

    update

--- lua/config/_lsp_config.lua
+++ lua/config/_lsp_config.lua
@@ -43,10 +43,7 @@ M.config = function()
 	end
 end
 M.set_signature = function(bufnr)
-	require("lsp_signature").on_attach({
-		bind = true, -- This is mandatory, otherwise border config won't get registered.
-		handler_opts = { border = "single" },
-	}, bufnr)
+	require("lsp_signature").on_attach({}, bufnr)
 end
 
 M.set_keymap = function(client, bufnr)
@@ -172,27 +169,51 @@ end
 M.signature = function()
 	local safe_require = require("utils").safe_require
 	safe_require("lsp_signature", function(lspsignature)
-		lspsignature.setup({
-			bind = true,
-			doc_lines = 2,
-			floating_window = true,
-			fix_pos = false,
-			hint_enable = false,
-			hint_prefix = "  ", -- Panda for parameter
+		local cfg = {
+			debug = false, -- set to true to enable debug logging
+			log_path = "debug_log_file_path", -- debug log path
+			verbose = false, -- show debug line number
+
+			bind = true, -- This is mandatory, otherwise border config won't get registered.
+			-- If you want to hook lspsaga or other signature handler, pls set to false
+			doc_lines = 0, -- will show two lines of comment/doc(if there are more than two lines in doc, will be truncated);
+			-- set to 0 if you DO NOT want any API comments be shown
+			-- This setting only take effect in insert mode, it does not affect signature help in normal
+			-- mode, 10 by default
+
+			floating_window = true, -- show hint in a floating window, set to false for virtual text only mode
+
+			floating_window_above_cur_line = true, -- try to place the floating above the current line when possible Note:
+			-- will set to true when fully tested, set to false will use whichever side has more space
+			-- this setting will be helpful if you do not want the PUM and floating win overlap
+			fix_pos = false, -- set to true, the floating window will not auto-close until finish all parameters
+			hint_enable = true, -- virtual hint enable
+			hint_prefix = " ", -- Panda for parameter
 			hint_scheme = "String",
-			transpancy = 40,
-			hi_parameter = "Search",
-			floating_window_above_cur_line = true,
-			max_height = 22,
+			use_lspsaga = false, -- set to true if you want to use lspsaga popup
+			hi_parameter = "LspSignatureActiveParameter", -- how your parameter will be highlight
+			max_height = 12, -- max height of signature floating_window, if content is more than max_height, you can scroll down
+			-- to view the hiding contents
 			max_width = 120, -- max_width of signature floating_window, line will be wrapped if exceed max_width
+			transpancy = 20, -- set this value if you want the floating windows to be transpant (100 fully transpant), nil to disable(default)
 			handler_opts = {
-				border = "shadow", -- double, single, shadow, none
+				border = "single",   -- double, single, shadow, none
 			},
-			zindex = 10, -- by default it will be on top of all floating windows, set to 50 send it to bottom
+
+			always_trigger = false, -- sometime show signature on new line or in middle of parameter can be confusing, set it to false for #58
+
+			auto_close_after = nil, -- autoclose signature float win after x sec, disabled if nil.
+			extra_trigger_chars = {}, -- Array of extra characters that will trigger signature completion, e.g., {"(", ","}
+			zindex = 200, -- by default it will be on top of all floating windows, set to <= 50 send it to bottom
+
 			padding = "", -- character to pad on left and right of signature can be ' ', or '|'  etc
+
 			shadow_blend = 36, -- if you using shadow as border use this set the opacity
 			shadow_guibg = "Black", -- if you using shadow as border use this set the color e.g. 'Green' or '#121315'
-		})
+			timer_interval = 200, -- default timer check interval set to lower value if you want to reduce latency
+			toggle_key = nil, -- toggle signature on and off in insert mode,  e.g. toggle_key = '<M-x>'
+		}
+		lspsignature.setup(cfg)
 	end)
 end
 
