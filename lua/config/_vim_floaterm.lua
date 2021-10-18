7c56983661b6ab87161ba2d7b8e52c5e62a42005
PangPangPangPangPang <446964321@qq.com>
2021-10-18 14:19:34 +0800

    update

--- lua/config/_vim_floaterm.lua
+++ lua/config/_vim_floaterm.lua
@@ -1,14 +1,33 @@
 local M = {}
+M.default_open_status = false
+M.lazygit_open_status = false
+M.open_term = function()
+	M.default_open_status = not M.default_open_status
+	vim.cmd([[ exe "FloatermToggle default" ]])
+end
+M.open_lazygit = function()
+	M.lazygit_open_status = true
+	vim.cmd([[ exe "FloatermNew --width=0.8 --height=0.8 lazygit" ]])
+end
+
+M.close = function()
+	if M.default_open_status == true then
+		M.default_open_status = false
+		vim.cmd([[ exe "FloatermToggle default" ]])
+	elseif M.lazygit_open_status == true then
+		vim.cmd([[ exe "FloatermKill" ]])
+		M.lazygit_open_status = false
+	end
+end
 M.map = function()
-    local g = vim.g
-    local map = require('utils').map
-    vim.api.nvim_command('hi! link FloatermBorder Directory')
-    g.floaterm_title = '$1/$2'
-    map('n', '<F5>', ":FloatermToggle default<CR>", {silent = true})
-    map('t', '<F5>', "<C-\\><C-n>:FloatermToggle default<CR>", {silent = true})
-    map('t', '<esc>', "<C-\\><C-n>:FloatermKill<CR>", {silent = true})
-    map('n', '<leader>lg', ":FloatermNew --width=0.8 --height=0.8 lazygit<CR>",
-        {silent = true})
+	local g = vim.g
+	local map = require("utils").map
+	vim.api.nvim_command("hi! link FloatermBorder Directory")
+	g.floaterm_title = "$1/$2"
+	map("n", "<F5>", ":lua require('config._vim_floaterm').open_term()<CR>", { silent = true })
+	map("t", "<F5>", "<C-\\><C-n>:lua require('config._vim_floaterm').open_term()<CR>", { silent = true })
+	map("t", "<Esc>", "<C-\\><C-n>:lua require('config._vim_floaterm').close()<CR>", { silent = true })
+	map("n", "<leader>lg", ":lua require('config._vim_floaterm').open_lazygit()<CR>", { silent = true })
 end
 
 return M
