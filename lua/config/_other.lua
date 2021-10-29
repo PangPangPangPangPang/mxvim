5343dc19a07221cd587855375dcd245e0633b15c
PangPangPangPangPang <446964321@qq.com>
2021-10-29 10:59:31 +0800

    update

--- lua/config/_other.lua
+++ lua/config/_other.lua
@@ -1,22 +1,25 @@
 local M = {}
-M.textobj_setup = function ()
-    vim.defer_fn(function ()
-        vim.cmd([[ PackerLoad vim-textobj-user ]])
-    end, 200)
+M.textobj_setup = function()
+	vim.defer_fn(function()
+		vim.cmd([[ PackerLoad vim-textobj-user ]])
+	end, 200)
 end
-M.signature_setup = function ()
-    vim.defer_fn(function ()
-        vim.cmd([[ PackerLoad vim-signature ]])
-    end, 200)
+M.signature_setup = function()
+	vim.defer_fn(function()
+		vim.cmd([[ PackerLoad vim-signature ]])
+	end, 200)
 end
-M.colorizer_setup = function ()
-    vim.defer_fn(function ()
-        vim.cmd([[ PackerLoad nvim-colorizer.lua ]])
-    end, 200)
+M.colorizer_setup = function()
+	vim.defer_fn(function()
+		vim.cmd([[ PackerLoad nvim-colorizer.lua ]])
+	end, 200)
 end
-M.copilot_setup = function ()
-    vim.defer_fn(function ()
-        vim.cmd([[ PackerLoad copilot.vim ]])
-    end, 200)
+M.copilot_setup = function()
+	-- vim.defer_fn(function()
+	-- 	vim.cmd([[
+        -- inoremap <silent><expr> <C-j> copilot#Accept("")
+        -- let g:copilot_no_tab_map = 1
+        -- PackerLoad copilot.vim ]])
+	-- end, 200)
 end
-return M;
+return M
