6c8a0128f1839ae4be4b01035a6b9086ed963908
PangPangPangPangPang <446964321@qq.com>
2021-11-30 11:40:50 +0800

    update

--- lua/theme.lua
+++ lua/theme.lua
@@ -1,7 +1,7 @@
 mxvim.init_theme = false
 local M = {}
 M.theme = function(name, nick)
-    local ret = {
+	local ret = {
 		name,
 		opt = true,
 		setup = function()
@@ -17,54 +17,60 @@ M.theme = function(name, nick)
 			vim.cmd([[
                 hi! link NormalFloat cleared
             ]])
-            local colors = require('theme').theme_colors()
-            vim.cmd(string.format('hi! CmpItemAbbrMatch gui=bold guifg=%s', colors.red))
-            vim.cmd(string.format('hi! CmpItemKind gui=bold guifg=%s', colors.yellow))
-            vim.cmd(string.format('hi! CmpItemMenu gui=bold guifg=%s', colors.cyan))
-            vim.cmd(string.format('hi! CmpItemMenu gui=bold guifg=%s', colors.cyan))
-            vim.cmd(string.format('hi! PmenuSel guifg=none'))
-            vim.cmd(string.format('hi! MatchParen cterm=reverse gui=underline'))
+			local colors = require("theme").theme_colors()
+			vim.cmd(string.format("hi! CmpItemAbbrMatch gui=bold guifg=%s", colors.blue))
+			vim.cmd(string.format("hi! CmpItemAbbrMatchFuzzy gui=bold guifg=%s", colors.blue))
+			vim.cmd(string.format("hi! CmpItemMenu gui=bold guifg=%s", colors.cyan))
+			vim.cmd(string.format("highlight! CmpItemKindVariable guibg=NONE guifg=%s", colors.yellow))
+			vim.cmd(string.format("highlight! CmpItemKindInterface guibg=NONE guifg=%s", colors.orange))
+			vim.cmd(string.format("highlight! CmpItemKindText guibg=NONE guifg=%s", colors.darkblue))
+			vim.cmd(string.format("highlight! CmpItemKindFunction guibg=NONE guifg=%s", colors.magenta))
+			vim.cmd(string.format("highlight! CmpItemKindMethod guibg=NONE guifg=%s", colors.red))
+			vim.cmd(string.format("highlight! CmpItemKindKeyword guibg=NONE guifg=%s", colors.green))
+			-- vim.cmd(string.format('hi! CmpItemKind gui=bold guifg=%s', colors.yellow))
+			vim.cmd(string.format("hi! PmenuSel guifg=none"))
+			vim.cmd(string.format("hi! MatchParen cterm=reverse gui=underline"))
 		end,
 	}
-    if nick ~= nil then
-        ret['as'] = nick
-    end
-	return ret;
+	if nick ~= nil then
+		ret["as"] = nick
+	end
+	return ret
 end
 
 M.extract_nvim_hl = function(name)
-  local hl_str = vim.api.nvim_exec('highlight ' .. name, true)
-  return {
-    fg = hl_str:match('guifg=(#[0-9A-Fa-f]+)') or '#444444',
-    bg = hl_str:match('guibg=(#[0-9A-Fa-f]+)') or '#1E1E1E',
-    style = hl_str:match('gui=(#[0-9A-Fa-f]+)') or '',
-    name = name,
-  }
+	local hl_str = vim.api.nvim_exec("highlight " .. name, true)
+	return {
+		fg = hl_str:match("guifg=(#[0-9A-Fa-f]+)") or "#444444",
+		bg = hl_str:match("guibg=(#[0-9A-Fa-f]+)") or "#1E1E1E",
+		style = hl_str:match("gui=(#[0-9A-Fa-f]+)") or "",
+		name = name,
+	}
 end
 
 -- color: #FFFFFF
 -- percent >= 0 && percent < 1
 M.shade = function(color, percent)
 	if mxvim.background == "dark" then
-        local colors = require("colorscheme." .. mxvim.current_theme).colors()
-        local bg_r = tonumber(string.sub(colors.bg, 2, 3), 16)
-        local bg_g = tonumber(string.sub(colors.bg, 4, 5), 16)
-        local bg_b = tonumber(string.sub(colors.bg, 6, 7), 16)
-        local cur_r = tonumber(string.sub(color, 2, 3), 16)
-        local cur_g = tonumber(string.sub(color, 4, 5), 16)
-        local cur_b = tonumber(string.sub(color, 6, 7), 16)
+		local colors = require("colorscheme." .. mxvim.current_theme).colors()
+		local bg_r = tonumber(string.sub(colors.bg, 2, 3), 16)
+		local bg_g = tonumber(string.sub(colors.bg, 4, 5), 16)
+		local bg_b = tonumber(string.sub(colors.bg, 6, 7), 16)
+		local cur_r = tonumber(string.sub(color, 2, 3), 16)
+		local cur_g = tonumber(string.sub(color, 4, 5), 16)
+		local cur_b = tonumber(string.sub(color, 6, 7), 16)
 		local r = string.format("%02x", cur_r - (cur_r - bg_r) * percent)
 		local g = string.format("%02x", cur_g - (cur_g - bg_g) * percent)
 		local b = string.format("%02x", cur_b - (cur_b - bg_b) * percent)
 		return string.format("#%s%s%s", r, g, b)
 	else
-        local colors = require("colorscheme." .. mxvim.current_theme).colors()
-        local bg_r = tonumber(string.sub(colors.bg, 2, 3), 16)
-        local bg_g = tonumber(string.sub(colors.bg, 4, 5), 16)
-        local bg_b = tonumber(string.sub(colors.bg, 6, 7), 16)
-        local cur_r = tonumber(string.sub(color, 2, 3), 16)
-        local cur_g = tonumber(string.sub(color, 4, 5), 16)
-        local cur_b = tonumber(string.sub(color, 6, 7), 16)
+		local colors = require("colorscheme." .. mxvim.current_theme).colors()
+		local bg_r = tonumber(string.sub(colors.bg, 2, 3), 16)
+		local bg_g = tonumber(string.sub(colors.bg, 4, 5), 16)
+		local bg_b = tonumber(string.sub(colors.bg, 6, 7), 16)
+		local cur_r = tonumber(string.sub(color, 2, 3), 16)
+		local cur_g = tonumber(string.sub(color, 4, 5), 16)
+		local cur_b = tonumber(string.sub(color, 6, 7), 16)
 		local r = string.format("%02x", cur_r + (bg_r - cur_r) * percent)
 		local g = string.format("%02x", cur_g + (bg_g - cur_g) * percent)
 		local b = string.format("%02x", cur_b + (bg_b - cur_b) * percent)
