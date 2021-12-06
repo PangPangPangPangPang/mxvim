6cc40caaa424ae7fb296ba85b948d8fcb8ed6739
PangPangPangPangPang <446964321@qq.com>
2021-12-06 11:52:24 +0800

    update

--- lua/config/_tabby.lua
+++ lua/config/_tabby.lua
@@ -1,4 +1,5 @@
 local fn = vim.fn
+local api = vim.api
 local o = vim.o
 local M = {}
 M.setup = function()
@@ -12,14 +13,14 @@ M.config = function ()
     local line = require("tabby.presets").active_wins_at_tail
     local label = function(tabid)
         return {
-            '  ' .. fn.getcwd(-1, tabid) .. ' ',
+            '  ' .. fn.getcwd(-1, api.nvim_tabpage_get_number(tabid)) .. ' ',
             hl = 'Substitute',
         }
     end
     line.active_tab.label = label
     local inactive_label = function(tabid)
         return {
-            '  ' .. fn.getcwd(-1, tabid) .. ' ',
+            '  ' .. fn.getcwd(-1, api.nvim_tabpage_get_number(tabid)) .. ' ',
             hl = 'TabLine',
         }
     end
