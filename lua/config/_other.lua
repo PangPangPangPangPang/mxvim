cabf4d43f108682b194298c28f71e14e7ffaac43
PangPangPangPangPang <446964321@qq.com>
2022-01-13 10:34:58 +0800

    update

--- lua/config/_other.lua
+++ lua/config/_other.lua
@@ -39,8 +39,8 @@ M.setup_lightspeed = function ()
 	vim.defer_fn(function()
 		vim.cmd([[
             PackerLoad lightspeed.nvim
-            silent! unmap s
-            silent! unmap S
+            silent! nunmap s
+            silent! nunmap S
             nmap <expr> f reg_recording() . reg_executing() == "" ? "<Plug>Lightspeed_f" : "f"
             nmap <expr> F reg_recording() . reg_executing() == "" ? "<Plug>Lightspeed_F" : "F"
             nmap <expr> t reg_recording() . reg_executing() == "" ? "<Plug>Lightspeed_t" : "t"
