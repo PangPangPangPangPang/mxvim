c4eeb154a1f4e8912d34ba6414b20253604a506e
PangPangPangPangPang <446964321@qq.com>
2022-04-24 17:09:34 +0800

    update

--- lua/config/_scrollbar.lua
+++ lua/config/_scrollbar.lua
@@ -1,13 +1,14 @@
 local M = {}
+local colors = require("theme").shade_colors(0.8)
 M.setup = function()
 	vim.defer_fn(function()
+        vim.cmd(string.format("highlight! ScrollBarHighlight guibg=%s guifg=NONE", colors.fg_green))
 		vim.cmd([[
             PackerLoad nvim-scrollbar
         ]])
 	end, 1000)
 end
 M.config = function()
-	local colors = require("theme").shade_colors(0.6)
 	require("scrollbar").setup({
 		show = true,
 		set_highlights = true,
@@ -15,7 +16,7 @@ M.config = function()
 			text = " ",
 			color = nil,
 			cterm = nil,
-			highlight = "Search",
+			highlight = "ScrollBarHighlight",
 			hide_if_all_visible = true, -- Hides handle if all lines are visible
 		},
 		marks = {
@@ -31,28 +32,28 @@ M.config = function()
 				priority = 1,
 				color = nil,
 				cterm = nil,
-				highlight = "DiagnosticVirtualTextError",
+				highlight = "DiagnosticSignError",
 			},
 			Warn = {
 				text = { "-", "=" },
 				priority = 2,
 				color = nil,
 				cterm = nil,
-				highlight = "DiagnosticVirtualTextWarn",
+				highlight = "DiagnosticSignWarn",
 			},
 			Info = {
 				text = { "-", "=" },
 				priority = 3,
 				color = nil,
 				cterm = nil,
-				highlight = "DiagnosticVirtualTextInfo",
+				highlight = "DiagnosticSignInfo",
 			},
 			Hint = {
 				text = { "-", "=" },
 				priority = 4,
 				color = nil,
 				cterm = nil,
-				highlight = "DiagnosticVirtualTextHint",
+				highlight = "DiagnosticSignHint",
 			},
 			Misc = {
 				text = { "-", "=" },
@@ -87,41 +88,5 @@ M.config = function()
 			search = false, -- Requires hlslens to be loaded, will run require("scrollbar.handlers.search").setup() for you
 		},
 	})
-	-- require("scrollbar").setup({
-	-- 	handle = {
-	-- 		text = " ",
-	-- 		color = colors.bg,
-	-- 		hide_if_all_visible = true, -- Hides handle if all lines are visible
-	-- 	},
-	-- 	marks = {
-	-- 		Search = { text = { "-", "=" }, priority = 0, color = colors.orange },
-	-- 		Error = { text = { "-", "=" }, priority = 1, color = colors.red },
-	-- 		Warn = { text = { "-", "=" }, priority = 2, color = colors.yellow },
-	-- 		Info = { text = { "-", "=" }, priority = 3, color = colors.blue },
-	-- 		Hint = { text = { "-", "=" }, priority = 4, color = colors.green },
-	-- 		Misc = { text = { "-", "=" }, priority = 5, color = colors.purple },
-	-- 	},
-	-- 	excluded_filetypes = {
-	-- 		"",
-	-- 		"prompt",
-	-- 		"TelescopePrompt",
-	-- 	},
-	-- 	autocmd = {
-	-- 		render = {
-	-- 			"BufWinEnter",
-	-- 			"TabEnter",
-	-- 			"TermEnter",
-	-- 			"WinEnter",
-	-- 			"CmdwinLeave",
-	-- 			"TextChanged",
-	-- 			"VimResized",
-	-- 			"WinScrolled",
-	-- 		},
-	-- 	},
-	-- 	handlers = {
-	-- 		diagnostic = true,
-	-- 		search = false,
-	-- 	},
-	-- })
 end
 return M
