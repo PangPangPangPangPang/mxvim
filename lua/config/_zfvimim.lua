275ddeb0b344a11151ac67245303c617f5fbb3d0
PangPangPangPangPang <446964321@qq.com>
2023-01-16 19:19:02 +0800

    fix key

--- lua/config/_zfvimim.lua
+++ lua/config/_zfvimim.lua
@@ -2,12 +2,18 @@
 local M = {}
 local is_zf_load = false
 M.setup = function()
-	vim.g.ZFVimIM_keymap = 0
-	vim.cmd([[
+    vim.g.ZFVimIM_keymap = 0
+    vim.cmd([[
         let g:ZFVimIM_symbolMap = {'`' : ['·'],'!' : ['！'],'$' : ['￥'],'^' : ['……'],'-' : [''],'_' : ['——'],'(' : ['（'],')' : ['）'],'[' : ['【'],']' : ['】'],'<' : ['《'],'>' : ['》'],'\' : ['、'],'/' : ['、'],';' : ['；'],':' : ['：'],',' : ['，'],'.' : ['。'],'?' : ['？'],"'" : ['‘', '’'],'"' : ['“', '”'],}
     ]])
+    -- vim.api.nvim_create_autocmd({ "User TelescopeFindPre" }, {
+    --     callback = function(ev)
+    --         stop_zfvimim()
+    --     end
+    -- })
 end
 function stop_zfvimim()
+    print('abc')
     if is_zf_load then
         vim.fn.ZFVimIME_stop()
     end
@@ -15,26 +21,23 @@ end
 
 function toggle_zfvimim()
     is_zf_load = true
-	vim.fn.ZFVimIME_keymap_toggle_i()
+    vim.fn.ZFVimIME_keymap_toggle_i()
 end
 
 M.config = function()
-	M.setLocalDB()
-	vim.cmd([[
-        autocmd User TelescopeFindPre lua stop_zfvimim()
-    ]])
+    M.setLocalDB()
 end
 
 M.setLocalDB = function()
-	local fn = vim.fn
-	if require("utils").is_module_available("private") == true then
-		local db = fn.ZFVimIM_dbInit({ name = "MaxDB" })
-		vim.fn.ZFVimIM_cloudRegister({
-			mode = "local",
-			dbId = db["dbId"],
-			repoPath = "~/.config/nvim",
-			dbFile = "/pinyin.txt",
-		})
-	end
+    local fn = vim.fn
+    if require("utils").is_module_available("private") == true then
+        local db = fn.ZFVimIM_dbInit({ name = "MaxDB" })
+        vim.fn.ZFVimIM_cloudRegister({
+            mode = "local",
+            dbId = db["dbId"],
+            repoPath = "~/.config/nvim",
+            dbFile = "/pinyin.txt",
+        })
+    end
 end
 return M
