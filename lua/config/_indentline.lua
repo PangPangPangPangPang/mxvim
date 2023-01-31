395b0cd8e2103e8b0fc7ae7d61f07e7c495b4168
PangPangPangPangPang <446964321@qq.com>
2023-01-31 13:51:59 +0800

    update

--- lua/config/_indentline.lua
+++ lua/config/_indentline.lua
@@ -7,12 +7,15 @@ M.setup = function ()
         "statement", "switch_body"
     }
     local colors = require('theme').shade_colors_default()
+    local commonColors = require('theme').theme_colors()
     local indent1 = string.format('hi! IndentBlanklineIndent1 guifg=%s blend=nocombine', colors.red);
     local indent2 = string.format('hi! IndentBlanklineIndent2 guifg=%s blend=nocombine', colors.yellow);
     local indent3 = string.format('hi! IndentBlanklineIndent3 guifg=%s blend=nocombine', colors.green);
     local indent4 = string.format('hi! IndentBlanklineIndent4 guifg=%s blend=nocombine', colors.cyan);
     local indent5 = string.format('hi! IndentBlanklineIndent5 guifg=%s blend=nocombine', colors.blue);
     local indent6 = string.format('hi! IndentBlanklineIndent6 guifg=%s blend=nocombine', colors.purple);
+    local contextChar = string.format('hi! IndentBlanklineContextChar guifg=%s blend=nocombine', commonColors.purple);
+    local contextStart = string.format('hi! IndentBlanklineContextStart gui=underline term=underline guisp=%s', commonColors.purple);
     vim.cmd(string.format([[
         augroup IndentBlankline
             %s
@@ -21,14 +24,17 @@ M.setup = function ()
             %s
             %s
             %s
+            %s
+            %s
         augroup End
-    ]], indent1, indent2, indent3, indent4, indent5, indent6))
+    ]], indent1, indent2, indent3, indent4, indent5, indent6, contextChar, contextStart))
+    vim.cmd [[
+    ]]
     require("indent_blankline").setup {
         char = '▏',
         buftype_exclude = {'tex', 'markdown', 'txt', 'startify', 'packer', 'terminal'},
         filetype_exclude = {'NvimTree', 'TelescopePrompt', 'mason'},
         show_end_of_line = true,
-        space_char_blankline = " ",
         char_highlight_list = {
             "IndentBlanklineIndent1",
             "IndentBlanklineIndent2",
@@ -37,7 +43,9 @@ M.setup = function ()
             "IndentBlanklineIndent5",
             "IndentBlanklineIndent6",
         },
-        -- show_current_context = true,
+        space_char_blankline = " ",
+        show_current_context = true,
+        show_current_context_start = true,
     }
 end
 M.setupIndent = function()
