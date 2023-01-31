5d58474f6a5b06cc1c5b32eb543bbcad8922ad53
PangPangPangPangPang <446964321@qq.com>
2023-01-31 16:20:39 +0800

    update

--- lua/colorscheme/nordic.lua
+++ lua/colorscheme/nordic.lua
@@ -1,28 +1,27 @@
-
 local M = {}
 M.config = function()
-    require(mxvim.current_theme) .load()
+    require(mxvim.current_theme).load()
 
 end
 
 M.colors = function()
-    local colors = require 'nordic.colors'
-	return {
-		fg = "#DCD7BA",
-		bg = "#16161D",
-		light_fb = "#717C7C",
-		light_bg = "#1F1F28",
-		fg_green = "#76946A",
-		yellow = "#E6C384",
-		cyan = "#7AA89F",
-		darkblue = "#658594",
-		green = "#98BB6C",
-		orange = "#FFA066",
-		purple = "#957FB8",
-		magenta = "#D27E99",
-		blue = colors.blue0,
-		red = "#FF5D62",
-        line_fg = "#16161D",
-	}
+    local nord = {
+        fg = "#ECEFF4",
+        bg = "#2E3440",
+        light_fg = "#ECEFF4",
+        light_bg = "#2E3440",
+        fg_green = "#A3BE8C",
+        yellow = "#EBCB8B",
+        cyan = "#8FBCBB",
+        darkblue = "#81A1C1",
+        green = "#A3BE8C",
+        orange = "#D08770",
+        purple = "#B48EAD",
+        magenta = "#D27E99",
+        blue = "#5E81AC",
+        red = "#BF616A",
+        line_fg = "#2E3440",
+    }
+    return nord
 end
 return M
