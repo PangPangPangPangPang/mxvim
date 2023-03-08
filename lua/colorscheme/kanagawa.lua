f79fb5f83848b26e15738f6c739f7e51a3b74ee8
PangPangPangPangPang <446964321@qq.com>
2023-03-08 11:08:42 +0800

    update

--- lua/colorscheme/kanagawa.lua
+++ lua/colorscheme/kanagawa.lua
@@ -10,7 +10,7 @@ M.config = function()
         verthoriz = "╋",
     })
     require('kanagawa').setup({
-        compile = false, -- enable compiling the colorscheme
+        compile = true, -- enable compiling the colorscheme
         undercurl = true, -- enable undercurls
         commentStyle = { italic = true },
         functionStyle = {},
@@ -18,7 +18,7 @@ M.config = function()
         statementStyle = { bold = true },
         typeStyle = {},
         transparent = false, -- do not set background color
-        dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+        dimInactive = true, -- dim inactive window `:h hl-NormalNC`
         terminalColors = true, -- define vim.g.terminal_color_{0,17}
         colors = { -- add/modify theme and palette colors
             palette = {},
