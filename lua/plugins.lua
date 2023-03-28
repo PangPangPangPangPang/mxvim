3176e274f4bfb3f43d43529b5213cd3d8ab3237d
PangPangPangPangPang <446964321@qq.com>
2023-03-28 17:53:24 +0800

    update

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -524,4 +524,33 @@ require("lazy").setup({
             vim.g.smartim_default = "com.apple.keylayout.ABC"
         end
     },
+    {
+            "jackMort/ChatGPT.nvim",
+            config = function()
+                    require("chatgpt").setup({
+                            keymaps = {
+                                    close = { "<C-c>" },
+                                    submit = "<C-e>",
+                                    yank_last = "<C-y>",
+                                    yank_last_code = "<C-k>",
+                                    scroll_up = "<C-u>",
+                                    scroll_down = "<C-d>",
+                                    toggle_settings = "<C-o>",
+                                    new_session = "<C-n>",
+                                    cycle_windows = "<Tab>",
+                                    -- in the Sessions pane
+                                    select_session = "<Space>",
+                                    rename_session = "r",
+                                    delete_session = "d",
+                            },
+
+                            -- optional configuration
+                    })
+            end,
+            dependencies = {
+                    "MunifTanjim/nui.nvim",
+                    "nvim-lua/plenary.nvim",
+                    "nvim-telescope/telescope.nvim"
+            }
+    }
 })
