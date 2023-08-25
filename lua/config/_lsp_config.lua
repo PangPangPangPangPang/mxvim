75e4c2fc92a6801b8040c02d906e682936432305
wangyefeng <wangyefeng@bytedance.com>
2023-08-25 11:19:55 +0800

    update

--- lua/config/_lsp_config.lua
+++ lua/config/_lsp_config.lua
@@ -3,99 +3,100 @@ local colors = require("theme").shade_colors(0.4)
 local cmd = vim.cmd
 
 M.config = function()
-    M.custom_handlers()
-    M.set_keymap()
+	M.custom_handlers()
+	M.set_keymap()
 end
 M.set_signature = function(bufnr)
-    local safe_require = require("utils").safe_require
-    safe_require("lsp_signature", function(lsp_signature)
-        lsp_signature.on_attach({}, bufnr)
-    end)
+	local safe_require = require("utils").safe_require
+	safe_require("lsp_signature", function(lsp_signature)
+		lsp_signature.on_attach({}, bufnr)
+	end)
 end
 
 M.set_keymap = function()
-    vim.api.nvim_create_autocmd("LspAttach", {
-        desc = "General LSP Attach",
-        callback = function(args)
-            local bufnr = args.buf
-            local client = vim.lsp.get_client_by_id(args.data.client_id)
-            local function buf_set_option(...)
-                vim.api.nvim_buf_set_option(bufnr, ...)
-            end
+	vim.api.nvim_create_autocmd("LspAttach", {
+		desc = "General LSP Attach",
+		callback = function(args)
+			local bufnr = args.buf
+			local client = vim.lsp.get_client_by_id(args.data.client_id)
+			local function buf_set_option(...)
+				vim.api.nvim_buf_set_option(bufnr, ...)
+			end
 
-            M.set_diagnostic(client)
+			M.set_diagnostic(client)
 
-            buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
+			buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
 
-            -- Setup keymaps
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = bufnr, desc = "LSP: Hover" })
-            vim.keymap.set({ "n", "i" }, "<C-k>", vim.lsp.buf.signature_help,
-                { buffer = bufnr, desc = "LSP: Signature help" })
-            vim.keymap.set("n", "[e", vim.diagnostic.goto_prev, { buffer = bufnr, desc = "Diagnostic" })
-            vim.keymap.set("n", "]e", vim.diagnostic.goto_next, { buffer = bufnr, desc = "Diagnostic" })
+			-- Setup keymaps
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = bufnr, desc = "LSP: Hover" })
+			vim.keymap.set({ "n", "i" }, "<C-k>", vim.lsp.buf.signature_help,
+				{ buffer = bufnr, desc = "LSP: Signature help" })
+			vim.keymap.set("n", "[e", vim.diagnostic.goto_prev, { buffer = bufnr, desc = "Diagnostic" })
+			vim.keymap.set("n", "]e", vim.diagnostic.goto_next, { buffer = bufnr, desc = "Diagnostic" })
 
-            vim.keymap.set("n", "<c-]>", vim.lsp.buf.definition, { buffer = bufnr, desc = "Definition" })
-            vim.keymap.set("n", "gd", vim.lsp.buf.declaration, { buffer = bufnr, desc = "Declaration" })
-            vim.keymap.set("n", "gI", vim.lsp.buf.implementation, { buffer = bufnr, desc = "Implementation" })
-            vim.keymap.set("n", "gr", function()
-                vim.lsp.buf.references { include_declaration = false }
-            end, { buffer = bufnr, desc = "References" })
+			vim.keymap.set("n", "<c-]>", vim.lsp.buf.definition, { buffer = bufnr, desc = "Definition" })
+			vim.keymap.set("n", "gd", vim.lsp.buf.declaration, { buffer = bufnr, desc = "Declaration" })
+			vim.keymap.set("n", "gI", vim.lsp.buf.implementation, { buffer = bufnr, desc = "Implementation" })
+			vim.keymap.set("n", "gr", function()
+				vim.lsp.buf.references { include_declaration = false }
+			end, { buffer = bufnr, desc = "References" })
 
-            vim.keymap.set("n", "<leader>cn", vim.lsp.buf.rename, { buffer = bufnr, desc = "Rename" })
-            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { buffer = bufnr, desc = "Code action" })
-            vim.keymap.set("n", "<leader>ll", vim.diagnostic.setloclist, { buffer = bufnr, desc = "Diagnostic list" })
-            vim.keymap.set("n", "<leader>ld", vim.diagnostic.open_float, { buffer = bufnr, desc = "Diagnostic float" })
+			vim.keymap.set("n", "<leader>cn", vim.lsp.buf.rename, { buffer = bufnr, desc = "Rename" })
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { buffer = bufnr, desc = "Code action" })
+			vim.keymap.set("n", "<leader>ll", vim.diagnostic.setloclist, { buffer = bufnr, desc = "Diagnostic list" })
+			vim.keymap.set("n", "<leader>ld", vim.diagnostic.open_float, { buffer = bufnr, desc = "Diagnostic float" })
 
-            vim.keymap.set("n", "<leader>cp", function()
-                vim.lsp.buf.format { async = true }
-            end, { buffer = bufnr, desc = "Format document" })
+			vim.keymap.set("n", "<leader>cp", function()
+				vim.lsp.buf.format { async = true }
+			end, { buffer = bufnr, desc = "Format document" })
 
-            vim.keymap.set(
-                "n",
-                "<leader>wa",
-                vim.lsp.buf.add_workspace_folder,
-                { buffer = bufnr, desc = "Add workspace folder" }
-            )
-            vim.keymap.set(
-                "n",
-                "<leader>wr",
-                vim.lsp.buf.remove_workspace_folder,
-                { buffer = bufnr, desc = "Remove workspace folder" }
-            )
-            vim.keymap.set("n", "<leader>wl", function()
-                print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-            end, { buffer = bufnr, desc = "List workspace folders" })
-            if vim.fn.has("nvim-1.0") == 1 then
-                if client and client.server_capabilities.inlayHintProvider then
-                    vim.lsp.inlay_hint(bufnr, true)
-                end
-            end
-        end,
-    })
+			vim.keymap.set(
+				"n",
+				"<leader>wa",
+				vim.lsp.buf.add_workspace_folder,
+				{ buffer = bufnr, desc = "Add workspace folder" }
+			)
+			vim.keymap.set(
+				"n",
+				"<leader>wr",
+				vim.lsp.buf.remove_workspace_folder,
+				{ buffer = bufnr, desc = "Remove workspace folder" }
+			)
+			vim.keymap.set("n", "<leader>wl", function()
+				print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+			end, { buffer = bufnr, desc = "List workspace folders" })
+			if vim.fn.has("nvim-1.0") == 1 then
+				if client and client.server_capabilities.inlayHintProvider then
+					vim.lsp.inlay_hint(bufnr, true)
+				end
+			end
+		end,
+	})
 end
+
 M.set_diagnostic = function(client)
-    -- Set autocommands conditional on server_capabilities
-    if client.server_capabilities.documentHighlightProvider then
-        local show_diag = "autocmd CursorHold * lua require('config._lsp_config').show_cursor_virt_diagnostic()"
-        cmd([[ augroup lsp_document_highlight ]])
-        cmd([[ autocmd! * <buffer> ]])
-        cmd([[ autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()  ]])
-        cmd([[ autocmd CursorHoldI <buffer> lua vim.lsp.buf.document_highlight() ]])
-        cmd([[ autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references() ]])
-        cmd(show_diag)
-        cmd([[ autocmd CursorMoved <buffer> lua require('config._lsp_config').hide_cursor_diagnostic() ]])
-        cmd([[ autocmd CursorMovedI <buffer> lua require('config._lsp_config').hide_cursor_diagnostic() ]])
-        -- cmd([[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync(nil, 1000)]])
-        cmd([[ augroup END ]])
-        cmd([[
+	-- Set autocommands conditional on server_capabilities
+	if client.server_capabilities.documentHighlightProvider then
+		local show_diag = "autocmd CursorHold * lua require('config._lsp_config').show_cursor_virt_diagnostic()"
+		cmd([[ augroup lsp_document_highlight ]])
+		cmd([[ autocmd! * <buffer> ]])
+		cmd([[ autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()  ]])
+		cmd([[ autocmd CursorHoldI <buffer> lua vim.lsp.buf.document_highlight() ]])
+		cmd([[ autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references() ]])
+		cmd(show_diag)
+		cmd([[ autocmd CursorMoved <buffer> lua require('config._lsp_config').hide_cursor_diagnostic() ]])
+		cmd([[ autocmd CursorMovedI <buffer> lua require('config._lsp_config').hide_cursor_diagnostic() ]])
+		-- cmd([[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync(nil, 1000)]])
+		cmd([[ augroup END ]])
+		cmd([[
             hi! default link DiagnosticVirtualTextError Comment
             hi! default link DiagnosticVirtualTextHint Comment
             hi! default link DiagnosticVirtualTextInfo Comment
             hi! default link DiagnosticVirtualTextWarn Comment
         ]])
 
-        cmd(string.format(
-            [[
+		cmd(string.format(
+			[[
         hi! DiagnosticDefaultInfo guifg=%s
         hi! DiagnosticUnderlineError gui=undercurl term=undercurl guisp=%s guifg=none
         hi! DiagnosticUnderlineHint gui=undercurl term=undercurl guisp=%s guifg=none
@@ -110,156 +111,145 @@ M.set_diagnostic = function(client)
         highlight! link LspReferenceRead LspReference
         highlight! link LspReferenceWrite LspReference
         ]],
-            colors.bg,
-            colors.red,
-            colors.blue,
-            colors.yellow,
-            colors.blue,
-            colors.red,
-            colors.blue,
-            colors.yellow,
-            colors.blue,
-            colors.highlight ~= nil and colors.highlight or "Visual"
-        ))
-    end
+			colors.bg,
+			colors.red,
+			colors.blue,
+			colors.yellow,
+			colors.blue,
+			colors.red,
+			colors.blue,
+			colors.yellow,
+			colors.blue,
+			colors.highlight ~= nil and colors.highlight or "Visual"
+		))
+	end
 end
 
 M.on_attach = function(client, bufnr)
-    M.set_signature(bufnr)
+	M.set_signature(bufnr)
 end
 
 M.make_config = function()
-    local safe_require = require("utils").safe_require
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
-    if mxvim.use_cmp == true then
-        safe_require("cmp_nvim_lsp", function(cmp)
-            capabilities = cmp.default_capabilities(capabilities)
-        end)
-    end
-    local root_dir
-    safe_require("lspconfig/util", function(util)
-        root_dir = util.root_pattern("package.json", ".eslintrc", ".git")
-    end)
-    local config = {
-        autostart = true,
-        root_dir = root_dir,
-        -- enable snippet support
-        capabilities = capabilities,
-        -- map buffer local keybindings when the language server attaches
-        on_attach = M.on_attach,
-    }
-    if mxvim.use_coq == true then
-        safe_require("coq", function(coq)
-            config = coq.lsp_ensure_capabilities(config)
-        end)
-    end
-    return config
+	local safe_require = require("utils").safe_require
+	local capabilities = vim.lsp.protocol.make_client_capabilities()
+	if mxvim.use_cmp == true then
+		safe_require("cmp_nvim_lsp", function(cmp)
+			capabilities = cmp.default_capabilities(capabilities)
+		end)
+	end
+	local root_dir
+	safe_require("lspconfig/util", function(util)
+		root_dir = util.root_pattern("package.json", ".eslintrc", ".git")
+	end)
+	local config = {
+		autostart = true,
+		root_dir = root_dir,
+		-- enable snippet support
+		capabilities = capabilities,
+		-- map buffer local keybindings when the language server attaches
+		on_attach = M.on_attach,
+	}
+	if mxvim.use_coq == true then
+		safe_require("coq", function(coq)
+			config = coq.lsp_ensure_capabilities(config)
+		end)
+	end
+	return config
 end
 
 M.custom_handlers = function()
-    local sign = function(opts)
-        vim.fn.sign_define(opts.name, {
-            texthl = opts.name,
-            text = "",
-            numhl = opts.name,
-        })
-    end
-    sign({ name = "DiagnosticSignError", text = " " })
-    sign({ name = "DiagnosticSignWarn", text = " " })
-    sign({ name = "DiagnosticSignHint", text = " " })
-    sign({ name = "DiagnosticSignInfo", text = " " })
+	local sign = function(opts)
+		vim.fn.sign_define(opts.name, {
+			texthl = opts.name,
+			text = "",
+			numhl = opts.name,
+		})
+	end
+	sign({ name = "DiagnosticSignError", text = " " })
+	sign({ name = "DiagnosticSignWarn", text = " " })
+	sign({ name = "DiagnosticSignHint", text = " " })
+	sign({ name = "DiagnosticSignInfo", text = " " })
 
-    local safe_require = require("utils").safe_require
-    safe_require("lsputil.locations", function(locations)
-        vim.lsp.handlers["textDocument/references"] = locations.references_handler
-        vim.lsp.handlers["textDocument/definition"] = locations.definition_handler
-        vim.lsp.handlers["textDocument/declaration"] = locations.declaration_handler
-        vim.lsp.handlers["textDocument/typeDefinition"] = locations.typeDefinition_handler
-        vim.lsp.handlers["textDocument/implementation"] = locations.implementation_handler
-    end)
-    safe_require("lsputilsymbols", function(symbols)
-        vim.lsp.handlers["textDocument/documentSymbol"] = symbols.document_handler
-        vim.lsp.handlers["workspace/symbol"] = symbols.workspace_handler
-    end)
+	local safe_require = require("utils").safe_require
+	safe_require("lsputil.locations", function(locations)
+		vim.lsp.handlers["textDocument/references"] = locations.references_handler
+		vim.lsp.handlers["textDocument/definition"] = locations.definition_handler
+		vim.lsp.handlers["textDocument/declaration"] = locations.declaration_handler
+		vim.lsp.handlers["textDocument/typeDefinition"] = locations.typeDefinition_handler
+		vim.lsp.handlers["textDocument/implementation"] = locations.implementation_handler
+	end)
+	safe_require("lsputilsymbols", function(symbols)
+		vim.lsp.handlers["textDocument/documentSymbol"] = symbols.document_handler
+		vim.lsp.handlers["workspace/symbol"] = symbols.workspace_handler
+	end)
 
-    local border = {
-        { "╭", "FloatBorder" },
-        { "─", "FloatBorder" },
-        { "╮", "FloatBorder" },
-        { "│", "FloatBorder" },
-        { "╯", "FloatBorder" },
-        { "─", "FloatBorder" },
-        { "╰", "FloatBorder" },
-        { "│", "FloatBorder" },
-    }
-    -- vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = border })
-    -- vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = border })
-    vim.diagnostic.config({
-        virtual_text = false,
-        -- virtual_text = {
-        -- 	spacing = 2,
-        -- },
-        signs = function()
-            return true
-        end,
-        underline = true,
-        update_in_insert = false,
-        float = {
-            show_header = true,
-            focusable = false,
-            border = "rounded",
-            source = "always",
-        },
-    })
+	vim.diagnostic.config({
+		virtual_text = false,
+		-- virtual_text = {
+		-- 	spacing = 2,
+		-- },
+		signs = function()
+			return true
+		end,
+		underline = true,
+		update_in_insert = false,
+		float = {
+			severity_sort = true,
+			source = "if_many",
+			show_header = true,
+			focusable = false,
+			border = "rounded"
+		},
+	})
 end
 
 _G.show_line_diagnostics_namespace = vim.api.nvim_create_namespace("show_line_diagnostics_namespace")
 
 M.hide_cursor_diagnostic = function()
-    local namespace = _G.show_line_diagnostics_namespace
-    vim.api.nvim_buf_clear_namespace(0, namespace, 0, -1)
+	local namespace = _G.show_line_diagnostics_namespace
+	vim.api.nvim_buf_clear_namespace(0, namespace, 0, -1)
 end
 
 M.show_cursor_virt_diagnostic = function()
-    local pos = vim.api.nvim_win_get_cursor(0)
-    local lnum = pos[1] - 1
-    local col = pos[2]
-    local diagnostics = vim.diagnostic.get(0, { lnum = lnum })
-    if vim.tbl_isempty(diagnostics) then
-        return
-    end
-    local line_length = #vim.api.nvim_buf_get_lines(0, lnum, lnum + 1, true)[1]
-    diagnostics = vim.tbl_filter(function(d)
-        return d.lnum == lnum and math.min(d.col, line_length - 1) <= col and (d.end_col >= col or d.end_lnum > lnum)
-    end, diagnostics)
-    if next(diagnostics) == nil then
-        return
-    end
-    local lines = {}
-    for _, diagnostic in ipairs(diagnostics) do
-        local prefix = string.format("%s: ", diagnostic.source)
-        local message_lines = vim.split(diagnostic.message, "\n")
-        table.insert(lines, prefix .. message_lines[1])
-        for j = 2, #message_lines do
-            table.insert(lines, string.rep(" ", #prefix) .. message_lines[j])
-        end
-    end
-    local namespace = _G.show_line_diagnostics_namespace
+	local pos = vim.api.nvim_win_get_cursor(0)
+	local lnum = pos[1] - 1
+	local col = pos[2]
+	local diagnostics = vim.diagnostic.get(0, { lnum = lnum })
+	if vim.tbl_isempty(diagnostics) then
+		return
+	end
+	local line_length = #vim.api.nvim_buf_get_lines(0, lnum, lnum + 1, true)[1]
+	diagnostics = vim.tbl_filter(function(d)
+		return d.lnum == lnum and math.min(d.col, line_length - 1) <= col and (d.end_col >= col or d.end_lnum > lnum)
+	end, diagnostics)
+	if next(diagnostics) == nil then
+		return
+	end
+	local lines = {}
+	for _, diagnostic in ipairs(diagnostics) do
+		local prefix = string.format("%s: ", diagnostic.source)
+		local message_lines = vim.split(diagnostic.message, "\n")
+		table.insert(lines, prefix .. message_lines[1])
+		for j = 2, #message_lines do
+			table.insert(lines, string.rep(" ", #prefix) .. message_lines[j])
+		end
+	end
+	local namespace = _G.show_line_diagnostics_namespace
 
-    local raw_line = ""
-    for _, line in ipairs(lines) do
-        raw_line = string.format("%s ■ %s", raw_line, line)
-    end
-    vim.api.nvim_buf_clear_namespace(0, namespace, 0, -1)
-    vim.api.nvim_buf_set_extmark(0, namespace, lnum, -1, {
-        virt_text = { { raw_line, "Comment" } },
-        virt_text_pos = "eol",
-        hl_mode = "combine",
-    })
+	local raw_line = ""
+	for _, line in ipairs(lines) do
+		raw_line = string.format("%s ■ %s", raw_line, line)
+	end
+	vim.api.nvim_buf_clear_namespace(0, namespace, 0, -1)
+	vim.api.nvim_buf_set_extmark(0, namespace, lnum, -1, {
+		virt_text = { { raw_line, "Comment" } },
+		virt_text_pos = "eol",
+		hl_mode = "combine",
+	})
 end
 
 M.show_cursor_diagnostic = function()
-    vim.diagnostic.open_float(nil, { focusable = false, border = "rounded", scope = "cursor", source = "always" })
+	vim.diagnostic.open_float(nil, { focusable = false, border = "rounded", scope = "cursor", source = "always" })
 end
 
 return M
