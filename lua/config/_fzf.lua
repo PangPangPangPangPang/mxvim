39bc35b1af071ffe9741bb93111ecbd350358366
wangyefeng <wangyefeng@bytedance.com>
2023-08-25 14:32:19 +0800

    update

--- lua/config/_fzf.lua
+++ lua/config/_fzf.lua
@@ -2,6 +2,23 @@ local M = {}
 M.config = function()
 	local actions = require "fzf-lua.actions"
 	require('fzf-lua').setup {
+		"default",
+		fzf_colors = {
+			-- ["fg"]      = { "fg", "CursorLine" },
+			-- ["bg"]      = { "bg", "Normal" },
+			-- ["hl"]      = { "fg", "Comment" },
+			["fg+"]     = { "fg", "Normal" },
+			["bg+"]     = { "bg", "CursorLine" },
+			-- ["hl+"]     = { "fg", "Statement" },
+			["info"]    = { "fg", "PreProc" },
+			-- ["prompt"]  = { "fg", "Conditional" },
+			["pointer"] = { "fg", "ErrorMsg" },
+			-- ["marker"]  = { "fg", "Keyword" },
+			["spinner"] = { "fg", "ErrorMsg" },
+			-- ["header"]  = { "fg", "Comment" },
+			["gutter"]  = { "bg", "Normal" },
+		},
+
 		actions = {
 			files = {
 				["enter"]  = actions.file_edit,
@@ -13,9 +30,24 @@ M.config = function()
 			}
 		},
 		files = {
-			prompt = 'Files: ',
+			prompt = "   ",
+			file_icons = true,
+			cwd_prompt = false,
+		},
+		winopts = {
+			preview = {
+				layout = "vertical",
+			}
+		},
+		git = {
+			files = {
+				prompt = "asd"
+			}
 		}
+
 	}
+	vim.api.nvim_set_hl(0, "FzfLuaBorder", { link = "Directory" })
+	vim.api.nvim_set_hl(0, "FzfLuaTitle", { link = "Float" })
 	local lmap = require("utils").map
 	lmap("n", "<c-p>", ":lua require('fzf-lua').files()<cr>", { silent = true })
 end
