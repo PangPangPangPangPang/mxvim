82a8eddcdbd647cc3bafaadcf49696857c6b71bf
Max <446964321@qq.com>
2023-08-27 13:05:59 +0800

    update

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -51,14 +51,14 @@ require("lazy").setup({
 	{
 		"folke/noice.nvim",
 		opts = {
-			-- lsp = {
-			-- 	-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-			-- 	override = {
-			-- 		["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-			-- 		["vim.lsp.util.stylize_markdown"] = true,
-			-- 		["cmp.entry.get_documentation"] = true,
-			-- 	},
-			-- },
+			lsp = {
+				-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+				override = {
+					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+					["vim.lsp.util.stylize_markdown"] = true,
+					["cmp.entry.get_documentation"] = true,
+				},
+			},
 			-- you can enable a preset for easier configuration
 			presets = {
 				bottom_search = true, -- use a classic bottom cmdline for search
