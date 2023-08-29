25a7db2a33529d43ac9775223a337162a781dac1
wangyefeng <wangyefeng@bytedance.com>
2023-08-30 00:41:49 +0800

    update

--- lua/mxvim.lua
+++ lua/mxvim.lua
@@ -1,6 +1,6 @@
 _G.mxvim = {}
 -- nightfly github-theme miramare vscode nord onedarkpro rose-pine catppuccin gruvbox-material gruvbox-baby kanagawa nightfox tundra oh-lucy oh-lucy-evening melange monokai
-mxvim.current_theme = "rose-pine"
+mxvim.current_theme = "kanagawa"
 -- galaxy lualine
 mxvim.current_line = "lualine"
 mxvim.use_coc = false
