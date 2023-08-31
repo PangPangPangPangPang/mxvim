7e2d290288853c54cd086faee56f25fda659da74
wangyefeng <wangyefeng@bytedance.com>
2023-08-31 17:50:53 +0800

    update

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -157,6 +157,7 @@ return {
 		"levouh/tint.nvim",
 		config = function()
 			require("tint").setup({
+				saturation = 0.6,  -- Saturation to preserve
 				transforms = require("tint").transforms.SATURATE_TINT,    -- Showing default behavior, but value here can be predefined set of transforms
 				tint_background_colors = false,                           -- Tint background portions of highlight groups
 				highlight_ignore_patterns = { "WinSeparator", "Status.*", "LineNr" }, -- Highlight group patterns to ignore, see `string.find`
