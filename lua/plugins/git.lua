16954a3eb1de59b5ac3ac38f43c71a70f2927bcb
wangyefeng <wangyefeng@bytedance.com>
2023-09-03 15:03:07 +0800

    update

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -64,8 +64,8 @@ return {
 						["o"] = "Toggle",
 						["a"] = "Stage",
 						["="] = "Toggle",
-						["p"] = "PullPopup",
-						["P"] = "PushPopup",
+						["P"] = "PullPopup",
+						["p"] = "PushPopup",
 					}
 				}
 			})
