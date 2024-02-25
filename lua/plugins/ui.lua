33948e8958475fb94907a4862c0187cd126913c6
Max <max@MaxdeMac-mini.local>
2024-02-26 01:15:56 +0800

    update

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -191,9 +191,9 @@ return {
 		lazy = true,
 		config = function()
 			require("smoothcursor").setup({
-				cursor = "▷ ",
+				-- cursor = "▷ ",
 				-- cursor = '▶ '
-				-- cursor = '⫸ '
+				cursor = '⫸ ',
 				disable_float_win = true,
 				always_redraw = false, -- Redraw the screen on each update
 				speed = 100,           -- Max speed is 100 to stick with your current position
