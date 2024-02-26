5b7ec427d50bb8fb593365162521e6f4e514495c
PangPangPangPangPang <446964321@qq.com>
2024-02-26 13:09:50 +0800

    update

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -194,11 +194,13 @@ return {
       require("smoothcursor").setup({
         -- cursor = "▷ ",
         -- cursor = '▶ '
-        cursor = "⫸ ",
+        -- cursor = "⫸ ",
+				cursor = "㊡",
 				disabled_filetypes = {"NeogitStatus"},   -- Disable for these file types, ignored if enabled_filetypes is set. e.g., { "TelescopePrompt", "NvimTree" }
         disable_float_win = true,
         always_redraw = false, -- Redraw the screen on each update
         speed = 100, -- Max speed is 100 to stick with your current position
+				priority = 99,              -- Set marker priority
       })
     end,
   },
