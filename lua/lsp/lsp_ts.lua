b12798b33bd70488ced8b26e6bc20ffd3b675073
Max <max@MaxdeMac-mini.local>
2024-02-27 20:30:08 +0800

    update

--- lua/lsp/lsp_ts.lua
+++ lua/lsp/lsp_ts.lua
@@ -1,6 +1,6 @@
 return {
   "pmizio/typescript-tools.nvim",
-	enabled = mxvim.enable_ts_tools,
+  enabled = mxvim.enable_ts_tools,
   event = {
     "BufRead *.js,*.jsx,*.mjs,*.cjs,*ts,*tsx",
     "BufNewFile *.js,*.jsx,*.mjs,*.cjs,*ts,*tsx",
@@ -16,33 +16,13 @@ return {
     },
   },
   opts = {
-    on_attach = function(client, bufnr)
-      client.server_capabilities.documentFormattingProvider = false
-      client.server_capabilities.documentRangeFormattingProvider = false
-      client.server_capabilities["textDocument/publishDiagnostics"] = function(err, result, ctx, config)
-        require("ts-error-translator").translate_diagnostics(err, result, ctx, config)
-        vim.lsp.diagnostic.on_publish_diagnostics(err, result, ctx, config)
-      end
-
-      vim.keymap.set("n", "gD", "<Cmd>TSToolsGoToSourceDefinition<CR>", { buffer = bufnr, desc = "Source Definition" })
-
-      vim.keymap.set(
-        "n",
-        "<leader>cI",
-        "<Cmd>TSToolsAddMissingImports<CR>",
-        { buffer = bufnr, desc = "Add missing imports" }
-      )
-      vim.keymap.set(
-        "n",
-        "<leader>co",
-        "<Cmd>TSToolsOrganizeImports<CR>",
-        { buffer = bufnr, desc = "Organize imports" }
-      )
-      vim.keymap.set("n", "<leader>cf", "<Cmd>TSToolsFixAll<CR>", { buffer = bufnr, desc = "Fix all" })
-    end,
+    handlers = {
+      ["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+        update_in_insert = true,
+      }),
+    },
     settings = {
       expose_as_code_action = { "fix_all", "add_missing_imports", "remove_unused" },
-
       tsserver_file_preferences = {
         includeCompletionsForModuleExports = true,
         includeInlayParameterNameHints = "all",
@@ -54,12 +34,6 @@ return {
         includeInlayFunctionLikeReturnTypeHints = true,
         includeInlayEnumMemberValueHints = true,
         quotePreference = "auto",
-      },
-      handlers = {
-        ["textDocument/publishDiagnostics"] = function(err, result, ctx, config)
-          require("ts-error-translator").translate_diagnostics(err, result, ctx, config)
-          vim.lsp.diagnostic.on_publish_diagnostics(err, result, ctx, config)
-        end,
       },
     },
   },
