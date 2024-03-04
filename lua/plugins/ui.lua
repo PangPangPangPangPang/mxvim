88b0f4cf868434b3cc0fec2f706ef0c899e7bfcd
PangPangPangPangPang <446964321@qq.com>
2024-03-04 16:54:45 +0900

    update

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -203,7 +203,7 @@ return {
         -- cursor = '▶ '
         -- cursor = "⫸ ",
         -- cursor = "㊡",
-        disabled_filetypes = { "NeogitStatus" }, -- Disable for these file types, ignored if enabled_filetypes is set. e.g., { "TelescopePrompt", "NvimTree" }
+        disabled_filetypes = { "NeogitStatus", "NvimTree", "fzf" }, -- Disable for these file types, ignored if enabled_filetypes is set. e.g., { "TelescopePrompt", "NvimTree" }
         disable_float_win = true,
         always_redraw = false, -- Redraw the screen on each update
         speed = 100, -- Max speed is 100 to stick with your current position
