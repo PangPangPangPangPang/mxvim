04d9a386da2d42b687b0eb8b60b61c88b892c788
Max <max@MaxdeMac-mini.local>
2024-03-10 17:50:38 +0800

    update

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -142,29 +142,6 @@ return {
     end,
   },
   {
-    "Exafunction/codeium.vim",
-    enabled = mxvim.enable_codeium,
-    event = "BufEnter",
-    config = function()
-      vim.g.codeium_no_map_tab = 1
-      vim.keymap.set("n", "<leader>cc", function()
-        return vim.fn["codeium#Chat"]()
-      end, { expr = true, silent = true, desc = "Chat with codeium" })
-      vim.keymap.set("i", "<C-Enter>", function()
-        return vim.fn["codeium#Accept"]()
-      end, { expr = true, silent = true })
-      vim.keymap.set("i", "<c-;>", function()
-        return vim.fn["codeium#CycleCompletions"](1)
-      end, { expr = true, silent = true })
-      vim.keymap.set("i", "<c-'>", function()
-        return vim.fn["codeium#CycleCompletions"](-1)
-      end, { expr = true, silent = true })
-      -- vim.keymap.set("i", "<c-x>", function()
-      --   return vim.fn["codeium#Clear"]()
-      -- end, { expr = true, silent = true })
-    end,
-  },
-  {
     "dnlhc/glance.nvim",
     event = "VeryLazy",
     config = function()
