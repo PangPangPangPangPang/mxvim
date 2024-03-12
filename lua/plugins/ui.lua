a21b957ee57bf153487efffa84de0171a4caf7cd
PangPangPangPangPang <446964321@qq.com>
2024-03-12 14:40:31 +0800

    update

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -9,52 +9,13 @@ return {
   {
     "folke/noice.nvim",
     event = "VeryLazy",
-    opts = {
-      cmdline = {
-        format = {
-          search_down = {
-            view = "cmdline",
-          },
-          search_up = {
-            view = "cmdline",
-          },
-        },
-      },
-      lsp = {
-        progress = {
-          enabled = false,
-        },
-        signature = {
-          enabled = true,
-          auto_open = {
-            enabled = true,
-            trigger = true, -- Automatically show signature help when typing a trigger character from the LSP
-            luasnip = true, -- Will open signature help when jumping to Luasnip insert nodes
-            throttle = 50, -- Debounce lsp signature help request by 50ms
-          },
-          view = nil, -- when nil, use defaults from documentation
-          opts = {}, -- merged with defaults from documentation
-        },
-        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-        override = {
-          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-          ["vim.lsp.util.stylize_markdown"] = true,
-          ["cmp.entry.get_documentation"] = true,
-        },
-      },
-      -- you can enable a preset for easier configuration
-      presets = {
-        bottom_search = false, -- use a classic bottom cmdline for search
-        command_palette = true, -- position the cmdline and popupmenu together
-        long_message_to_split = true, -- long messages will be sent to a split
-        inc_rename = false, -- enables an input dialog for inc-rename.nvim
-        lsp_doc_border = true, -- add a border to hover docs and signature help
-      },
-    },
     dependencies = {
       -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
       "MunifTanjim/nui.nvim",
     },
+		config = function ()
+			require("config._noice").setup()
+		end
   },
   {
     "luochen1990/rainbow",
