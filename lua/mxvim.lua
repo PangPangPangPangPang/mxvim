bab63c8b3a1c8446767e16dd22222125ef8d39e8
Max <max@MaxdeMacBook-Pro.local>
2024-03-16 16:27:49 +0800

    update

--- lua/mxvim.lua
+++ lua/mxvim.lua
@@ -3,9 +3,9 @@ _G.mxvim = {}
 -- nightfly github-theme miramare vscode nord onedarkpro rose-pine catppuccin
 -- gruvbox-material gruvbox-baby kanagawa nightfox tundra oh-lucy oh-lucy-evening
 -- melange monokai tokyonight oxocarbon bamboo modus
-mxvim.current_theme = "kanagawa"
+mxvim.current_theme = "bamboo"
 -- light / dark
-mxvim.background = "light"
+mxvim.background = "dark"
 -- simple / pure / normal
 mxvim.style = "simple"
 mxvim.use_coc = false
