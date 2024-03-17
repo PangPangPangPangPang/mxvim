df3e78ab6eed5fa6c20dde4ebc53d6fc58587dbb
Max <max@MaxdeMacBook-Pro.local>
2024-03-17 13:35:46 +0800

    update

--- lua/config/_other.lua
+++ lua/config/_other.lua
@@ -54,7 +54,7 @@ M.ufo_opts = function()
 			return { "indent" }
 		end,
 		open_fold_hl_timeout = 400,
-		close_fold_kinds = { "imports", "comment" },
+		close_fold_kinds_for_ft = { "imports", "comment" },
 		preview = {
 			win_config = { border = { "", "─", "", "", "", "─", "", "" }, winblend = 0 },
 			mappings = {
