8ca5843bdefa06bd7ce4308c4d15101ada979a71
Max <max@MaxdeMacBook-Pro.local>
2024-04-25 15:18:14 +0800

    `Update Ollama model to llama3 in ai.lua plugin`

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -62,7 +62,7 @@ return {
 			-- uncomment the following lines if you want to use Ollama:
 			vim.g.gpt_commit_engine = "ollama"
 			vim.g.gpt_commit_ollama_url = "http://127.0.0.1:11434/api/chat"
-			vim.g.gpt_commit_ollama_model = "llama2"
+			vim.g.gpt_commit_ollama_model = "llama3"
 			vim.g.gpt_commit_concise = 1
 		end,
 	},
