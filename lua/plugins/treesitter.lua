542bdf4f4d5839388bc6c1e8b285f428edf12ccf
Max <max@MaxdeMacBook-Pro.local>
2024-05-16 09:43:58 +0800

    update

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -24,47 +24,11 @@ return {
     end,
   },
   {
-    "lukas-reineke/headlines.nvim",
-    enabled = mxvim.enable_treesitter,
+    "MeanderingProgrammer/markdown.nvim",
     ft = { "markdown" },
-    -- ft = mxvim.treesitter_filetype,
-    dependencies = "nvim-treesitter/nvim-treesitter",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
     config = function()
-      require("headlines").setup({
-        markdown = {
-          query = vim.treesitter.query.parse(
-            "markdown",
-            [[
-                (atx_heading [
-                    (atx_h1_marker)
-                    (atx_h2_marker)
-                    (atx_h3_marker)
-                    (atx_h4_marker)
-                    (atx_h5_marker)
-                    (atx_h6_marker)
-                ] @headline)
-
-                (thematic_break) @dash
-
-                (fenced_code_block) @codeblock
-
-                (block_quote_marker) @quote
-                (block_quote (paragraph (inline (block_continuation) @quote)))
-                (block_quote (paragraph (block_continuation) @quote))
-                (block_quote (block_continuation) @quote)
-            ]]
-          ),
-          headline_highlights = { "Headline" },
-          codeblock_highlight = "CodeBlock",
-          dash_highlight = "Dash",
-          dash_string = "-",
-          quote_highlight = "Quote",
-          quote_string = "┃",
-          fat_headlines = true,
-          fat_headline_upper_string = "▃",
-          fat_headline_lower_string = "🬂",
-        },
-      })
-    end, -- or `opts = {}`
+      require("render-markdown").setup({})
+    end,
   },
 }
