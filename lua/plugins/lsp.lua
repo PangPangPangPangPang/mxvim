f63fb6dc61b2767d428dfa9d2ad8cc8a2fafa83b
Max <446964321@qq.com>
2024-12-03 09:52:54 +0800

    updat

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -44,15 +44,14 @@ return {
         preset = "enter",
         ["<C-e>"] = { "fallback" },
       },
-      highlight = {
-        ns = vim.api.nvim_create_namespace("blink_cmp"),
-        -- sets the fallback highlight groups to nvim-cmp's highlight groups
-        -- useful for when your theme doesn't support blink.cmp
-        -- will be removed in a future release, assuming themes add support
+      appearance = {
         use_nvim_cmp_as_default = true,
+        -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+        -- Adjusts spacing to ensure icons are aligned
+        nerd_font_variant = "mono",
       },
-      windows = {
-        autocomplete = {
+      completion = {
+        menu = {
           border = "rounded",
         },
         documentation = {
