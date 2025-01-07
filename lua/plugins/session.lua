ba3db8241f51ed56b6f329dbddf2eacbb0c3aa39
Max <446964321@qq.com>
2025-01-07 11:22:47 +0900

    update

--- lua/plugins/session.lua
+++ lua/plugins/session.lua
@@ -1,49 +1,64 @@
 return {
-  {
-    "goolord/alpha-nvim",
-    cmd = "Alpha",
-    dependencies = {
-      "nvim-tree/nvim-web-devicons",
-    },
-    init = function()
-      if vim.fn.argc() == 0 then
-        local alpha = require("alpha")
-        local dashboard = require("alpha.themes.dashboard")
-        dashboard.section.header.val = {
-          [[   __  ___                     _  __   __     _   __ _      ]],
-          [[  /  |/  /___ ___ __  _    __ (_)/ /_ / /    | | / /(_)__ _ ]],
-          [[ / /|_/ // _ `/\ \ / | |/|/ // // __// _ \   | |/ // //  ' \]],
-          [[/_/  /_/ \_,_//_\_\  |__,__//_/ \__//_//_/   |___//_//_/_/_/]],
-        }
-
-        dashboard.section.buttons.val = {
-          dashboard.button("<leader> S", "  Open Session", "<cmd>SessionManager load_session<CR>"),
-          dashboard.button("q", "󰅚  Quit NVIM", "<cmd>qa<CR>"),
-        }
-        local handle = io.popen("fortune")
-        if handle == nil then
-          return
-        end
-        local fortune = handle:read("*a")
-        handle:close()
-        dashboard.section.footer.val = fortune
-
-        dashboard.config.opts.noautocmd = true
-
-        vim.cmd([[autocmd User AlphaReady echo 'ready']])
-
-        alpha.setup(dashboard.config)
-      end
-    end,
-  },
-  {
-    "Shatur/neovim-session-manager",
-    cmd = { "SessionManager" },
-    dependencies = {
-      "nvim-telescope/telescope.nvim",
-    },
-    keys = {
-      { "<leader>s", "<cmd>SessionManager load_session<cr>", mode = "n", desc = "Load sessions" },
-    },
-  },
+	{
+		"folke/snacks.nvim",
+		---@type snacks.Config
+		opts = {
+			dashboard = {
+				sections = {
+					{
+						section = "terminal",
+						cmd = "pokemon-colorscripts -r --no-title; sleep .1",
+						random = 10,
+						pane = 1,
+						height = 30,
+						width = 40,
+					},
+					{
+						icon = " ",
+						key = "s",
+						desc = "Open Session",
+						action = "<cmd>SessionManager load_session<CR>",
+						pane = 2,
+						padding = 1,
+					},
+					{
+						icon = " ",
+						key = "f",
+						desc = "Find File",
+						action = ":lua Snacks.dashboard.pick('files')",
+						pane = 2,
+						padding = 1,
+					},
+					{
+						icon = "󰒲 ",
+						key = "L",
+						desc = "Lazy",
+						action = ":Lazy",
+						enabled = package.loaded.lazy ~= nil,
+						pane = 2,
+						padding = 1,
+					},
+					{
+						icon = " ",
+						key = "q",
+						desc = "Quit",
+						action = ":qa",
+						pane = 2,
+						padding = 1,
+					},
+					{ section = "startup", pane = 2 },
+				},
+			},
+		},
+	},
+	{
+		"Shatur/neovim-session-manager",
+		cmd = { "SessionManager" },
+		dependencies = {
+			"nvim-telescope/telescope.nvim",
+		},
+		keys = {
+			{ "<leader>s", "<cmd>SessionManager load_session<cr>", mode = "n", desc = "Load sessions" },
+		},
+	},
 }
