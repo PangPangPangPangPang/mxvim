ce006f1e702dad03081a273bc8038b10b4dae89f
maxwanggg <69065999+maxwanggg@users.noreply.github.com>
2025-02-13 08:18:11 +0800

    update

--- lua/plugins/basic.lua
+++ lua/plugins/basic.lua
@@ -4,5 +4,12 @@ return {
 	{ "drmikehenry/vim-fixkey" },
 	-- Readline style insertion
 	{ "tpope/vim-rsi" },
+	{
+		"nmac427/guess-indent.nvim",
+		event = "VeryLazy",
+		config = function()
+			require("guess-indent").setup({})
+		end,
+	},
 	{ "kevinhwang91/nvim-bqf", ft = "qf", branch = "main" },
 }
