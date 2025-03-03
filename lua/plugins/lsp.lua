fc32735d5a448daddfe59792e301d048ed1204f2
maxwanggg <69065999+maxwanggg@users.noreply.github.com>
2025-03-03 10:57:37 +0900

    update 				completion.menu.auto_show = true,

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -83,40 +83,14 @@ return {
           winblend = 30,
         },
       },
-      cmdline = {
-        enabled = true,
-        keymap = {
-          preset = "enter",
-          ["<Enter>"] = { "fallback" },
-          ["<C-e>"] = { "fallback" },
-          ["<C-n>"] = { "show", "select_next", "fallback" },
-          ["<Tab>"] = { "select_next", "fallback" },
-        }, -- Inherits from top level `keymap` config when not set
-        sources = function()
-          local type = vim.fn.getcmdtype()
-          -- Search forward and backward
-          if type == "/" or type == "?" then
-            return { "buffer" }
-          end
-          -- Commands
-          if type == ":" or type == "@" then
-            return { "cmdline" }
-          end
-          return {}
-        end,
-        completion = {
-          trigger = {
-            show_on_blocked_trigger_characters = {},
-            show_on_x_blocked_trigger_characters = nil, -- Inherits from top level `completion.trigger.show_on_blocked_trigger_characters` config when not set
-          },
-          menu = {
-            auto_show = nil, -- Inherits from top level `completion.menu.auto_show` config when not set
-            draw = {
-              columns = { { "label", "label_description", gap = 1 } },
-            },
-          },
-        },
-      },
+			cmdline = {
+				enabled = true,
+				completion = {
+					menu = {
+						auto_show = true,
+					}
+				}
+			},
     },
   },
   {
