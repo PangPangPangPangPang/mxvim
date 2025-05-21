b98e68008b8b90930cad5780bc8990cd3be50648
maxwanggg <69065999+maxwanggg@users.noreply.github.com>
2025-05-21 14:17:01 +0900

    update

--- nvim_setup.sh
+++ nvim_setup.sh
@@ -9,14 +9,15 @@ mkdir -p ${dst_dir} 2>/dev/null
 # rm -rf ~/.config/coc/
 
 input_db="$(pwd)/pinyin.txt"
-if [ ! -f ${input_db} ];then
-    curl https://raw.githubusercontent.com/PangPangPangPangPang/ZFVimIM_pinyin/master/misc/pinyin.txt >> ${input_db}
+if [ ! -f ${input_db} ]; then
+	curl https://raw.githubusercontent.com/PangPangPangPangPang/ZFVimIM_pinyin/master/misc/pinyin.txt >>${input_db}
 fi
 
 ln -s ${input_db} ${dst_dir}
 
 ln -s "$(pwd)/init.lua" "${dst_dir}/init.lua"
 ln -s "$(pwd)/lua" "${dst_dir}/lua"
+ln -s "$(pwd)/lsp" "${dst_dir}/lsp"
 ln -s "$(pwd)/viml" "${dst_dir}/viml"
 ln -s "$(pwd)/coc-settings.json" ${dst_dir}
 ln -s "$(pwd)/ginit.vim" ${dst_dir}
