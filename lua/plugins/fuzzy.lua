477f4d507456449b147bbae2942b93ff61df0934
maxwanggg <69065999+maxwanggg@users.noreply.github.com>
2025-05-30 18:02:02 +0900

    update

--- lua/plugins/fuzzy.lua
+++ lua/plugins/fuzzy.lua
@@ -20,6 +20,21 @@ return {
   {
     "folke/snacks.nvim",
     lazy = false,
+    init = function()
+      local prev = { new_name = "", old_name = "" } -- Prevents duplicate events
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "NvimTreeSetup",
+        callback = function()
+          local events = require("nvim-tree.api").events
+          events.subscribe(events.Event.NodeRenamed, function(data)
+            if prev.new_name ~= data.new_name or prev.old_name ~= data.old_name then
+              data = data
+              Snacks.rename.on_rename_file(data.old_name, data.new_name)
+            end
+          end)
+        end,
+      })
+    end,
     keys = {
       {
         "<leader>f",
@@ -124,19 +139,4 @@ return {
       },
     },
   },
-  config = function()
-    local prev = { new_name = "", old_name = "" } -- Prevents duplicate events
-    vim.api.nvim_create_autocmd("User", {
-      pattern = "NvimTreeSetup",
-      callback = function()
-        local events = require("nvim-tree.api").events
-        events.subscribe(events.Event.NodeRenamed, function(data)
-          if prev.new_name ~= data.new_name or prev.old_name ~= data.old_name then
-            data = data
-            Snacks.rename.on_rename_file(data.old_name, data.new_name)
-          end
-        end)
-      end,
-    })
-  end,
 }
