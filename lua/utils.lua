eed1f7d34325dfde3744c477c6960df422b8f024
maxwanggg <69065999+maxwanggg@users.noreply.github.com>
2025-10-26 16:56:51 +0900

    update

--- lua/utils.lua
+++ lua/utils.lua
@@ -202,45 +202,8 @@ M.load_plugins = function()
   end
 
   vim.opt.rtp:prepend(lazypath)
-
-  local ok, lazy = pcall(require, "lazy")
-  if not ok then
-    return
-  end
-
-  pcall(vim.api.nvim_create_user_command, "PS", function()
-    lazy.sync()
-  end, {})
-
-  lazy.setup({
-    spec = {
-      { import = "plugins" },
-    },
-    defaults = {
-      lazy = true,
-      version = false,
-    },
-    install = { missing = false },
-    checker = { enabled = false },
-    change_detection = { notify = false },
-    performance = {
-      cache = { enabled = true },
-      reset_packpath = true,
-      rtp = {
-        disabled_plugins = {
-          "gzip",
-          "tarPlugin",
-          "tohtml",
-          "tutor",
-          "zipPlugin",
-          "rplugin",
-          "matchit",
-          "matchparen",
-          "netrwPlugin",
-        },
-      },
-    },
-  })
+  vim.cmd([[ com! PS lua require('lazy').sync() ]])
+  require("lazy").setup("plugins")
 end
 
 M.is_alacritty = function()
